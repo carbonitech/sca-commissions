46d76dcf2252b5a7686a33914a220377d307ed1c
Joe Carboni <joe@carbonitech.com>
2022-06-03 21:51:37 -0400

    implemented test fot get_mapping. passed

--- app/routes/api_services.py
+++ app/routes/api_services.py
@@ -4,68 +4,77 @@ from app.db import db
 from typing import Dict
 
 # mappings
-def get_mapping_tables(db: db.Database) -> set:
+def get_mapping_tables(database: db.Database) -> set:
 
-    with db as db_conn:
+    with database as db_conn:
+        db_conn: db.Database
         all_tables = db_conn.get_tables()
 
     mapping_tables = {record[0] for record in all_tables if record[0].split("_")[0] == "map"}
     return mapping_tables
 
-def get_mapping(db: db.Database, table: int) -> pd.DataFrame:
+
+def get_mapping(database: db.Database, table: str) -> pd.DataFrame:
+    
+    with database as db_conn:
+        db_conn: db.Database
+        mapping_data_columns = db_conn.select_records(table="INFORMATION_SCHEMA.COLUMNS", columns=["column_name"], constraints={"TABLE_NAME": table})
+        mapping_data = db_conn.select_records(table=table)
+
+    return pd.DataFrame.from_records(mapping_data, columns=[col[0] for col in mapping_data_columns])
+
+
+def set_mapping(database: db.Database, table: int, data: pd.DataFrame) -> bool:
     ...
 
-def set_mapping(db: db.Database, table: int, data: pd.DataFrame) -> bool:
-    ...
-
-def del_mapping(db: db.Database, table: int, id: int) -> bool:
+def del_mapping(database: db.Database, table: int, id: int) -> bool:
     ...
 
 # final commission data
-def get_final_data(db: db.Database) -> pd.DataFrame:
+def get_final_data(database: db.Database) -> pd.DataFrame:
     ...
 
-def record_final_data(db: db.Database, data: pd.DataFrame) -> bool:
+def record_final_data(database: db.Database, data: pd.DataFrame) -> bool:
     ...
 
 # submission metadata
-def get_submissions_metadata(db: db.Database, manufacturer_id: int) -> pd.DataFrame:
+def get_submissions_metadata(database: db.Database, manufacturer_id: int) -> pd.DataFrame:
     ...
 
-def del_submission(db: db.Database, submission_id: int) -> bool:
+def del_submission(database: db.Database, submission_id: int) -> bool:
     ...
 
 # original submission files
-def get_submission_files(db: db.Database, manufacturer_id: int) -> Dict[int,str]:
+def get_submission_files(database: db.Database, manufacturer_id: int) -> Dict[int,str]:
     ...
 
-def record_submission_file(db: db.Database, manufactuer_id: int, file: bytes) -> bool:
+def record_submission_file(database: db.Database, manufactuer_id: int, file: bytes) -> bool:
     ...
 
-def del_submission_file(db: db.Database, id: int) -> bool:
+def del_submission_file(database: db.Database, id: int) -> bool:
     ...
 
 # processing steps log
-def get_processing_steps(db: db.Database, submission_id: int) -> pd.DataFrame:
+def get_processing_steps(database: db.Database, submission_id: int) -> pd.DataFrame:
     ...
 
-def record_processing_steps(db: db.Database, submission_id: int, data: pd.DataFrame) -> bool:
+def record_processing_steps(database: db.Database, submission_id: int, data: pd.DataFrame) -> bool:
     ...
 
-def del_processing_steps(db: db.Database, submission_id: int) -> bool:
+def del_processing_steps(database: db.Database, submission_id: int) -> bool:
     ...
 
 # errors
-def get_errors(db: db.Database, submission_id: int) -> pd.DataFrame:
+def get_errors(database: db.Database, submission_id: int) -> pd.DataFrame:
     ...
 
-def record_errors(db: db.Database, submission_id: int, data: pd.DataFrame) -> bool:
+def record_errors(database: db.Database, submission_id: int, data: pd.DataFrame) -> bool:
     ...
 
-def correct_error(db: db.Database, error_id: int, data: pd.DataFrame) -> bool:
+def correct_error(database: db.Database, error_id: int, data: pd.DataFrame) -> bool:
     ...
 
-def del_error(db: db.Database, error_id: int) -> bool:
+def del_error(database: db.Database, error_id: int) -> bool:
     ...
 
 
--- tests/routes_test.py
+++ tests/routes_test.py
@@ -1,6 +1,10 @@
 import unittest
 import dotenv
 import os
+from typing import Dict
+
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
 
 from app.db import db, tables
 from app.routes import api_services
@@ -21,13 +25,50 @@ class TestApiServiceFunctions(unittest.TestCase):
             for name, schema in self.tables.items():
                 db_conn.create_table(table_name=name, columns=schema)
 
+
     def test_get_mapping_tables(self):
 
-        mapping_tables: set = api_services.get_mapping_tables(db=self.sql_db)
+        mapping_tables: set = api_services.get_mapping_tables(database=self.sql_db)
         expected_tables: set = {table for table in TABLES if table.split("_")[0] == "map"}
 
         self.assertEqual(mapping_tables, expected_tables)
 
+
+    def test_get_mapping(self):
+
+        # test data to insert by table
+        mapping_table_entries: Dict[str,dict] = {
+            "map_customer_name": {
+                "recorded_name": "WICHITTEN SUPPLY",
+                "standard_name": "WITTICHEN SUPPLY"
+            },
+            "map_rep": {
+                "rep_id": 7,
+                "customer_branch_id": 32,
+            },
+            "map_city_names": {
+                "recorded_name": "FORREST PARK",
+                "standard_name": "FOREST PARK"
+            }
+        }
+
+        # create records to retrieve in each table
+        with self.sql_db as db_conn:
+            db_conn: db.SQLDatabase
+            for tbl, entry in mapping_table_entries.items():
+                db_conn.create_record(table=tbl, data=entry)
+
+        for tbl, entry in mapping_table_entries.items():
+            result = api_services.get_mapping(database=self.sql_db, table=tbl)
+            expected = pd.DataFrame({**{"id":[1]},**{k:[v] for k,v in entry.items()}}) # upgrading to python 3.10 could replace unpacking ** with a pipe | operator
+            assert_frame_equal(result,expected)
+    
+    
+    def test_set_mapping(self):
+        ...
+
+    def test_del_mapping(self):
+        ...
 
     def tearDown(self):
         with self.sql_db as db_conn:

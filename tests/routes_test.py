ff64dc08d8e262d30f75312cf056d57443a4a6e5
Joe Carboni <joe@carbonitech.com>
2022-06-09 08:08:27 -0400

    setup estabs db tbls w- data and expected dfs

--- tests/routes_test.py
+++ tests/routes_test.py
@@ -18,13 +18,13 @@ dotenv.load_dotenv()
 class TestApiServiceFunctions(unittest.TestCase):
 
     def setUp(self):
-        db_url = os.getenv("TESTING_DATABASE_URL")
+
         entries_by_table: Dict[str,dict] = {
             "map_customer_name": {
                 "recorded_name": ["MINGLEDROFFS", "EDSSUPPLYCO", "DSC"],
                 "standard_name": ["MINGLEDORFFS", "EDS SUPPLY COMPANY","DEALERS SUPPLY COMPANY"]
             },
-            "map_rep": {
+            "map_reps_customers": {
                 "rep_id": [6,6,2],
                 "customer_branch_id": [350,19,31],
             },
@@ -39,16 +39,52 @@ class TestApiServiceFunctions(unittest.TestCase):
                 "salesman": ["mwr","sca", "jdc", "red"],
                 "customer_name": ["Coastal Supply","Baker Distributing","Dealers Supply","Hinkle Metals"],
                 "city": ["Knoxville","Jacksonville","Forest Park","Birmingham"],
-                "state": ["TN","FL","GA","AL"],
-                "inv_amt": [randint(100,50000000)/100 for _ in range(0,4)]
+                "state": ["TN","FL","GA","AL"]
             }
         }
-        entries_by_table["final_commission_data"]["comm_amt"] = [round(num*3/100, 2) for num in entries_by_table["final_commission_data"]["inv_amt"]]
-        
+
+        inv_amts = [randint(100,50000000)/100 for _ in range(0,4)]
+        entries_by_table["final_commission_data"]["inv_amt"] = inv_amts
+        comm_amts = [round(num*3/100, 2) for num in inv_amts]
+        entries_by_table["final_commission_data"]["comm_amt"] = comm_amts
+
+        self.entries_dfs = {tbl_name:pd.DataFrame(data) for tbl_name,data in entries_by_table.items()}
+        for tbl,df in self.entries_dfs.items():
+             self.entries_dfs[tbl].insert(0,"id",list(range(1,len(df)+1))) 
+
+        db_url = os.getenv("TESTING_DATABASE_URL")
         self.db = create_engine(db_url)
         db.Base.metadata.create_all(self.db)
         with Session(self.db) as session:
             session.add(db.MapCustomerName(recorded_name='MINGLEDROFFS', standard_name='MINGLEDORFFS'))
+            session.add(db.MapCustomerName(recorded_name='EDSSUPPLYCO', standard_name='EDS SUPPLY COMPANY'))
+            session.add(db.MapCustomerName(recorded_name='DSC', standard_name='DEALERS SUPPLY COMPANY'))
+            session.add(db.MapRepsToCustomer(rep_id=6, customer_branch_id=350))
+            session.add(db.MapRepsToCustomer(rep_id=6, customer_branch_id=19))
+            session.add(db.MapRepsToCustomer(rep_id=2, customer_branch_id=31))
+            session.add(db.MapCityName(recorded_name="CHATNOGA", standard_name="CHATTANOOGA"))
+            session.add(db.MapCityName(recorded_name="PT_ST_LUCIE", standard_name="PORT SAINT LUCIE"))
+            session.add(db.MapCityName(recorded_name="BLUERIDGE", standard_name="BLUE RIDGE"))
+            session.add(db.FinalCommissionData(
+                year=2019, month="January", manufacturer="ADP", salesman="mwr",
+                customer_name="Coastal Supply", city="Knoville", state = "TN",
+                inv_amt=inv_amts[0], comm_amt=comm_amts[0]
+            ))
+            session.add(db.FinalCommissionData(
+                year=2020, month="November", manufacturer="Berry", salesman="sca",
+                customer_name="Baker Distributing", city="Jacksonville", state = "FL",
+                inv_amt=inv_amts[1], comm_amt=comm_amts[1]
+            ))
+            session.add(db.FinalCommissionData(
+                year=2021, month="April", manufacturer="Allied", salesman="jdc",
+                customer_name="Dealers Supply", city="Forest Park", state = "GA",
+                inv_amt=inv_amts[2], comm_amt=comm_amts[2]
+            ))
+            session.add(db.FinalCommissionData(
+                year=2022, month="July", manufacturer="Atco", salesman="red",
+                customer_name="Hinkle Metals", city="Birmingham", state = "AL",
+                inv_amt=inv_amts[3], comm_amt=comm_amts[3]
+            ))
             session.commit()
         return
 
@@ -59,12 +95,21 @@ class TestApiServiceFunctions(unittest.TestCase):
         self.assertEqual(result,expected)
 
     def test_get_mappings(self):
-        result = api_services.get_mappings(
-            conn=self.db,
-            table=db.MapCustomerName
-        )
-        expected = pd.DataFrame({"id":[1],"recorded_name":['MINGLEDROFFS'],"standard_name":['MINGLEDORFFS']})
-        assert_frame_equal(result,expected)
+
+        results = {
+            "map_customer_name": api_services.get_mappings(
+                conn=self.db,
+                table="map_customer_name"),
+            "map_city_names": api_services.get_mappings(
+                conn=self.db,
+                table="map_city_names"),
+            "map_reps_customers": api_services.get_mappings(
+                conn=self.db,
+                table="map_reps_customers")
+            }
+        for table,result in results.items():
+            expected = self.entries_dfs[table]
+            assert_frame_equal(result,expected)
 
 
     def test_set_mapping(self): self.assertTrue(False)

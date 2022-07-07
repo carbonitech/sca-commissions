c99584921a6a65aabf63166ea3a3fe04901373a9
Joe Carboni <joe@carbonitech.com>
2022-07-07 08:14:20 -0400

    add name and init 2 manf. mod attrs & init of sub

--- app/manufacturers/base.py
+++ app/manufacturers/base.py
@@ -1,11 +1,31 @@
 """Defines manufacturer and submission base classes"""
+import os
+import dotenv
+from sqlalchemy import create_engine
+import app.db.db_services as db_serv
+
+dotenv.load_dotenv()
+
+DB_URL = os.getenv("SCA_DATABASE_URL")
+DB_ENGINE = create_engine(DB_URL)
+
+database = db_serv.DatabaseServices(DB_ENGINE)
+
 
 class Manufacturer:
     """
     defines manufacturer-specific attributes
     and handles report processing execution
+
+    This is a base class for creating manufacturers
     """
-    pass
+
+    name = None
+
+    def __init__(self):
+        self.mappings = {table: database.get_mappings(table) for table in database.get_mapping_tables()}
+        self.id = database.get_manufacturer_id(self.name)
+        
     
 
 class Submission:
@@ -21,11 +41,8 @@ class Submission:
     id = None
     errors = None
     processing_steps = None
+    final_comm_data = None
+    total_comm = 0  # tracking cents, not dollars
 
-    def __init__(self, manufacturer: Manufacturer, file: bytes) -> None:
-        self.manufacturer = manufacturer
+    def __init__(self, file: bytes) -> None:
         self.file = file
-
-    def process(self):
-        return
-

1277a23a06eb72ac509bddbafc6ec33759354f9a
Joe Carboni <joe@carbonitech.com>
2022-07-07 17:55:40 -0400

    added Error class

--- app/manufacturers/base.py
+++ app/manufacturers/base.py
@@ -1,6 +1,7 @@
 """Defines manufacturer and submission base classes"""
 import os
 import dotenv
+from dataclasses import dataclass
 from sqlalchemy import create_engine
 import app.db.db_services as db_serv
 
@@ -38,11 +39,24 @@ class Submission:
     takes a Manufacturer object in the constructor to access
     manufacturer attributes and report processing procedures
     """
-    id = None
-    errors = None
-    processing_steps = None
+    errors = []
+    processing_steps = []
     final_comm_data = None
     total_comm = 0  # tracking cents, not dollars
 
-    def __init__(self, file: bytes) -> None:
+    def __init__(self, rep_mon: int, rep_year: int, file: bytes) -> None:
         self.file = file
+        self.report_month = rep_mon
+        self.report_year = rep_year
+        self.id = ...
+
+
+@dataclass
+class Error:
+    submission_id: int
+    row_index: int
+    field: str
+    value_type: type
+    value_content: str
+    reason: str
+    row_data: dict

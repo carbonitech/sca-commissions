b6ec358631f06303ce62e6c6ab1cdb69de23780c
Joe Carboni <joe@carbonitech.com>
2022-07-20 07:33:28 -0400

    moved helper funcs from adp subclass

--- app/manufacturers/base.py
+++ app/manufacturers/base.py
@@ -1,6 +1,7 @@
 """Defines manufacturer and submission base classes"""
 import os
 import dotenv
+from typing import Union
 from datetime import datetime
 from dataclasses import dataclass
 
@@ -33,6 +34,145 @@ class Manufacturer:
         self.customer_branches = database.get_customers_branches()
         self.reps_to_cust_branch_ref = database.get_reps_to_cust_branch_ref()
         
+
+    def fill_customer_ids(self, data: pd.DataFrame, column: Union[str,int]) -> pd.DataFrame:
+
+        data_cols = data.columns.tolist()
+        left_on_name = None
+
+        if isinstance(column,int):
+            left_on_name = data_cols[column]
+        elif isinstance(column,str) and column in data_cols:
+            left_on_name = column
+
+        if not left_on_name:
+            raise IndexError(f"column {column} is not in the dataset")
+            
+        customer_name_map = self.mappings["map_customer_name"]
+        merged_with_name_map = pd.merge(data, customer_name_map,
+                how="left", left_on=left_on_name, right_on="recorded_name")
+        
+        match_is_null = merged_with_name_map["recorded_name"].isnull()
+        no_match_table = merged_with_name_map[match_is_null]
+
+        # customer column is going from a name string to an id integer
+        data[left_on_name] = merged_with_name_map.loc[:,"customer_id"].fillna(0).astype(int)
+        
+        error_reason = "Customer name in the commission file is not mapped to a standard name"
+        self.record_mapping_errors(no_match_table, left_on_name, error_reason, str)
+
+        return data
+
+
+    def fill_city_ids(self, data: pd.DataFrame, column: Union[str,int]) -> pd.DataFrame:
+
+        data_cols = data.columns.tolist()
+        left_on_name = None
+
+        if isinstance(column,int):
+            left_on_name = data_cols[column]
+        elif isinstance(column,str) and column in data_cols:
+            left_on_name = column
+
+        if not left_on_name:
+            raise IndexError(f"column {column} is not in the dataset")
+            
+        merged_w_cities_map = pd.merge(
+            data, self.mappings["map_city_names"],
+            how="left", left_on=left_on_name, right_on="recorded_name"
+        )
+
+        match_is_null = merged_w_cities_map["recorded_name"].isnull()
+        no_match_table = merged_w_cities_map[match_is_null]
+
+        # city column is going from a name string to an id integer
+        data[left_on_name] = merged_w_cities_map.loc[:,"city_id"].fillna(0).astype(int)
+
+        error_reason = "City name in the commission file is not mapped to a standard name"
+        self.record_mapping_errors(no_match_table, left_on_name, error_reason, str)
+
+        return data
+
+
+    def fill_state_ids(self, data: pd.DataFrame, column: Union[str,int]) -> pd.DataFrame:
+        
+        data_cols = data.columns.tolist()
+        left_on_name = None
+
+        if isinstance(column,int):
+            left_on_name = data_cols[column]
+        elif isinstance(column,str) and column in data_cols:
+            left_on_name = column
+
+        if not left_on_name:
+            raise IndexError(f"column {column} is not in the dataset")
+
+        merged_w_states_map = pd.merge(
+            data, self.mappings["map_state_names"],
+            how="left", left_on=left_on_name, right_on="recorded_name"
+        )
+
+        match_is_null = merged_w_states_map["recorded_name"].isnull()
+        no_match_table = merged_w_states_map[match_is_null]
+
+        # state column is going from a name string to an id integer
+        data[left_on_name] = merged_w_states_map.loc[:,"state_id"].fillna(0).astype(int)
+
+        error_reason = "State name in the commission file is not mapped to a standard name"
+        self.record_mapping_errors(no_match_table, left_on_name, error_reason, str)
+
+        return data
+
+
+    def add_rep_customer_ids(self, data: pd.DataFrame, 
+            ref_columns: list, new_column: str = "map_rep_customer_id") -> pd.DataFrame:
+        
+        data_cols = data.columns.tolist()
+        left_on_list = []
+        for column in ref_columns:
+            if isinstance(column,int):
+                left_on_list.append(data_cols[column])
+            elif isinstance(column,str) and column in data_cols:
+                left_on_list.append(column)
+
+        if not left_on_list:
+            raise IndexError(f"one or more columns in {ref_columns} are not in the dataset")
+
+        merged_w_reference = pd.merge(
+            data, self.reps_to_cust_branch_ref,
+            how="left", left_on=left_on_list,
+            right_on=["customer_id","city_id","state_id"]
+        )
+
+        match_is_null = merged_w_reference["customer_id"].isnull()
+        no_match_table = merged_w_reference[match_is_null]
+
+        data[new_column] = merged_w_reference.loc[:,"map_rep_customer_id"].fillna(0).astype(int)
+
+        error_reason = "Customer does not have a branch association with the city and state listed"
+        self.record_mapping_errors(no_match_table, new_column, error_reason, int, 0)
+
+        return data
+
+
+    def record_mapping_errors(
+            self, data: pd.DataFrame, field: str, reason: str,
+            value_type, value_content: str = None):
+
+        for row_index, row_data in data.to_dict("index").items():
+            if not value_content:
+                value_content = row_data[field]
+            error_obj = Error(
+                submission_id=self.submission.id,
+                row_index=row_index,
+                field=field,
+                value_type=value_type,
+                value_content=0,
+                reason=reason,
+                row_data={row_index: row_data})
+
+            self.submission.errors.append(error_obj)
+
     
 
 class Submission:

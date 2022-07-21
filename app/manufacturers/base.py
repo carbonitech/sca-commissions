f4469fb93ae17092855fd74e6b890a3951678880
Joe Carboni <joe@carbonitech.com>
2022-07-21 10:44:46 -0400

    added ProcessingStep

--- app/manufacturers/base.py
+++ app/manufacturers/base.py
@@ -18,6 +18,38 @@ DB_ENGINE = create_engine(DB_URL)
 database = db_serv.DatabaseServices(DB_ENGINE)
 
 
+class Submission:
+    """
+    handles report processing:
+    tracks the state of submission attributes such as id,
+    errors, processing steps, etc. to use in post-processing
+    database operations
+
+    takes a Manufacturer object in the constructor to access
+    manufacturer attributes and report processing procedures
+    """
+    errors = []
+    processing_steps = []
+    final_comm_data = None
+    total_comm = 0  # tracking cents, not dollars
+
+    def __init__(self, rep_mon: int, rep_year: int, report_id: int, file: bytes, sheet_name: str=None) -> None:
+        self.file = file
+        self.report_month = rep_mon
+        self.report_year = rep_year
+        self.report_id = report_id
+        self.sheet_name = sheet_name
+        self.submission_date = datetime.today()
+        self.id = database.record_submission_metadata(
+                    report_id=self.report_id,
+                    data=pd.Series({
+                        "submission_date": self.submission_date,
+                        "reporting_month": self.report_month,
+                        "reporting_year": self.report_year
+                    })
+                )
+
+
 class Manufacturer:
     """
     defines manufacturer-specific attributes
@@ -28,11 +60,12 @@ class Manufacturer:
 
     name: str = None # defined by subclasses, manufacturer's db name
 
-    def __init__(self):
+    def __init__(self, submission: Submission):
         self.mappings = {table: database.get_mappings(table) for table in database.get_mapping_tables()}
         self.id = database.get_manufacturer_id(self.name)
         self.customer_branches = database.get_customers_branches()
         self.reps_to_cust_branch_ref = database.get_reps_to_cust_branch_ref()
+        self.submission = submission
         
 
     def fill_customer_ids(self, data: pd.DataFrame, column: Union[str,int]) -> pd.DataFrame:
@@ -172,39 +205,15 @@ class Manufacturer:
                 row_data={row_index: row_data})
 
             self.submission.errors.append(error_obj)
+        return
 
-    
 
-class Submission:
-    """
-    handles report processing:
-    tracks the state of submission attributes such as id,
-    errors, processing steps, etc. to use in post-processing
-    database operations
-
-    takes a Manufacturer object in the constructor to access
-    manufacturer attributes and report processing procedures
-    """
-    errors = []
-    processing_steps = []
-    final_comm_data = None
-    total_comm = 0  # tracking cents, not dollars
-
-    def __init__(self, rep_mon: int, rep_year: int, report_id: int, file: bytes, sheet_name: str=None) -> None:
-        self.file = file
-        self.report_month = rep_mon
-        self.report_year = rep_year
-        self.report_id = report_id
-        self.sheet_name = sheet_name
-        self.submission_date = datetime.today()
-        self.id = database.record_submission_metadata(
-                    report_id=self.report_id,
-                    data=pd.Series({
-                        "submission_date": self.submission_date,
-                        "reporting_month": self.report_month,
-                        "reporting_year": self.report_year
-                    })
-                )
+    def record_processing_step(self, step_description: str):
+        self.submission.processing_steps.append(
+            ProcessingStep(submission_id=self.submission.id, step_desctription=step_description)
+        )
+        ProcessingStep.increment_step_num()
+        return
 
 
 @dataclass
@@ -216,3 +225,20 @@ class Error:
     value_content: str
     reason: str
     row_data: dict
+
+
+class ProcessingStep:
+
+    step_num = 1 # overall processing step number is tracked by the class
+
+    def __init__(self, submission_id: int, step_desctription: str):
+        self.submission_id = submission_id
+        self.step_desctription = step_desctription
+        self.step_num = self.step_num # lock-in the step number on instantiation
+
+    @classmethod
+    def increment_step_num(cls):
+        cls.step_num += 1
+
+    def __str__(self) -> str:
+        return f"submission_id = {self.submission_id}, step_num = {self.step_num}, description = {self.step_desctription}"

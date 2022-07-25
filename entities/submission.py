676c8441681048d0630341fb6872ccbf2ef33720
Joe Carboni <joe@carbonitech.com>
2022-07-25 06:38:30 -0400

    rm registeredsubmission, conv file to object

--- entities/submission.py
+++ entities/submission.py
@@ -1,15 +1,16 @@
 
 from datetime import datetime
 from dataclasses import dataclass, field
+from pandas import DataFrame
+from entities.commission_file import CommissionFile
 
 @dataclass
 class NewSubmission:
-    file: bytes
+    file: CommissionFile
     report_month: int
     report_year: int
     report_id: int
     manufacturer_id: int
-    sheet_name: str
     submission_date: datetime = field(default_factory=datetime.today, init=False)
 
     def keys(self):
@@ -17,18 +18,7 @@ class NewSubmission:
         
     def __getitem__(self,key):
         return getattr(self,key)
+
+    def file_df(self) -> DataFrame:
+        return self.file.to_df()
     
-
-@dataclass
-class RegisteredSubmission:
-    id: int
-    report_month: int
-    report_year: int
-    report_id: int
-    submission_date: datetime
-
-    def keys(self):
-        return list(self.__dict__.keys())
-        
-    def __getitem__(self,key):
-        return getattr(self,key)

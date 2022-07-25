2a88c612bcdcb2d976c12130460f0cc8c814f730
Joe Carboni <joe@carbonitech.com>
2022-07-25 06:32:42 -0400

    new module to rep pre and post processed data

---
+++ entities/commission_data.py
@@ -0,0 +1,35 @@
+from typing import List
+from dataclasses import dataclass
+from pandas import DataFrame
+
+from entities.processing_step import ProcessingStep
+from entities.error import Error
+
+
+@dataclass
+class PreProcessedData:
+    data: DataFrame
+    process_steps: List[ProcessingStep]
+    map_rep_customer_ref_cols: List[str]
+    customer_name_col: str
+    city_name_col: str
+    state_name_col: str
+
+    def total_commission(self) -> int:
+        return self.data.loc[:,"comm_amt"].sum()
+
+    def total_sales(self) -> int:
+        return self.data.loc[:,"inv_amt"].sum()
+
+
+@dataclass
+class PostProcessedData:
+    data: DataFrame
+    process_steps: List[ProcessingStep]
+    errors: List[Error]
+
+    def total_commissions(self):
+        return round(self.data.loc[:,"comm_amt"].sum())/100
+
+    def total_sales(self):
+        return round(self.data.loc[:,"inv_amt"].sum())/100

de9d00df87970d18a6ceeed077e8e0ff16afd858
Joe Carboni <joe@carbonitech.com>
2022-07-26 09:43:50 -0400

    moved init from subclass to abc

--- entities/preprocessor.py
+++ entities/preprocessor.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 
+from entities.submission import NewSubmission
+
 class PreProcessor(ABC):
 
+    def __init__(self, submission: NewSubmission):
+        self.submission = submission
+        
     @abstractmethod
     def preprocess(self, submission):
         pass

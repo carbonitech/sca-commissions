c32ffca05d987199b9fa485abaa17f73bcb25149
Joe Carboni <joe@carbonitech.com>
2022-07-27 08:53:28 -0400

    mod attr names to match SubmissionDTO

--- entities/submission.py
+++ entities/submission.py
@@ -7,14 +7,17 @@ from entities.commission_file import CommissionFile
 @dataclass
 class NewSubmission:
     file: CommissionFile
-    report_month: int
-    report_year: int
+    reporting_month: int
+    reporting_year: int
     report_id: int
     manufacturer_id: int
     submission_date: datetime = field(default_factory=datetime.today, init=False)
 
     def keys(self):
-        return list(self.__dict__.keys())
+        unpackable_attrs = list(self.__dict__.keys())
+        unpackable_attrs.remove("file")
+        unpackable_attrs.remove("manufacturer_id")
+        return unpackable_attrs
         
     def __getitem__(self,key):
         return getattr(self,key)

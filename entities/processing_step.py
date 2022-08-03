929c352f2f0775f764f78c12f37ee94b9b388a5f
Joe Carboni <joe@carbonitech.com>
2022-08-02 20:07:01 -0400

    sub_id is inst arg instead of added by mthd

--- entities/processing_step.py
+++ entities/processing_step.py
@@ -1,9 +1,10 @@
 class ProcessingStep:
     total_steps = 1 # overall processing step number is tracked by the class
 
-    def __init__(self, description: str):
+    def __init__(self, description: str, sub_id: int):
         self.description = description
         self.step_num = self.total_steps
+        self.submission_id = sub_id
         ProcessingStep.increment_total_step_num()
 
     @classmethod
@@ -13,9 +14,6 @@ class ProcessingStep:
     @classmethod
     def set_total_step_num(cls, new_num):
         cls.total_steps = new_num
-
-    def add_submission_id(self, value: int) -> None:
-        self.submission_id = value
 
     def __str__(self) -> str:
         return f"step_num = {self.step_num}, description = {self.description}"

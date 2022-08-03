390ea16646ed464584b9a46f8ad6a10512f89130
Joe Carboni <joe@carbonitech.com>
2022-08-03 15:31:04 -0400

    report id and commissionfile as args

--- entities/preprocessor.py
+++ entities/preprocessor.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
 
-from entities.submission import NewSubmission
 from entities.commission_data import PreProcessedData
+from entities.commission_file import CommissionFile
 
 class PreProcessor(ABC):
 
-    def __init__(self, submission: NewSubmission, submission_id: int):
-        self.submission = submission
+    def __init__(self, report_id: int, submission_id: int, file: CommissionFile):
+        self.report_id = report_id
         self.submission_id = submission_id
+        self.file = file
         
     @abstractmethod
     def preprocess(self)-> PreProcessedData:

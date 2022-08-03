181384909e487d26163f8e727fe5473739748e46
Joe Carboni <joe@carbonitech.com>
2022-08-02 20:06:39 -0400

    accepts sub_id and abc method matches impl verison

--- entities/preprocessor.py
+++ entities/preprocessor.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 
 from entities.submission import NewSubmission
+from entities.commission_data import PreProcessedData
 
 class PreProcessor(ABC):
 
-    def __init__(self, submission: NewSubmission):
+    def __init__(self, submission: NewSubmission, submission_id: int):
         self.submission = submission
+        self.submission_id = submission_id
         
     @abstractmethod
-    def preprocess(self, submission):
+    def preprocess(self)-> PreProcessedData:
         pass

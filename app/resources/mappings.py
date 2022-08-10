0db442e0532f0070ea54c83e1f605116cc0e7cb7
Joe Carboni <joe@carbonitech.com>
2022-08-10 13:12:41 -0400

    added e-p's. impl all gets

--- app/resources/mappings.py
+++ app/resources/mappings.py
@@ -1,25 +1,49 @@
+import json
 from fastapi import APIRouter
+from db import db_services
 
 router = APIRouter(prefix="/mappings")
+db = db_services.DatabaseServices()
 
-### MAPPINGS ###
-@router.get("/mappings/customers")
-async def get_all_mappings_related_to_customers(): ...
 
-@router.get("/mappings/customers/{customer_id}")
-async def get_all_mappings_related_to_a_specific_customer(): ...
+@router.get("/customers")
+async def get_all_mappings_related_to_customers():
+    return json.loads(db.get_all_customer_name_mappings().to_json(orient="records"))
 
-@router.get("/mappings/cities")
-async def get_all_mappings_for_location_names(): ...
+@router.get("/customers/{customer_id}")
+async def get_all_mappings_related_to_a_specific_customer(customer_id: int):
+    return json.loads(db.get_all_customer_name_mappings(customer_id).to_json(orient="records"))
 
-@router.get("/mappings/states")
-async def get_all_mappings_for_location_names(): ...
+@router.get("/cities")
+async def get_all_mappings_for_city_names():
+    return json.loads(db.get_all_city_name_mappings().to_json(orient="records"))
 
-@router.post("/mappings/customers")
+@router.get("/cities/{city_id}")
+async def get_all_mappings_a_city(city_id: int):
+    return json.loads(db.get_all_city_name_mappings(city_id).to_json(orient="records"))
+
+@router.get("/states")
+async def get_all_mappings_for_state_names():
+    return json.loads(db.get_all_state_name_mappings().to_json(orient="records"))
+
+@router.get("/states/{state_id}")
+async def get_all_mappings_a_city(state_id: int):
+    return json.loads(db.get_all_state_name_mappings(state_id).to_json(orient="records"))
+
+@router.post("/customers")
 async def create_new_mapping_for_a_customer(): ...
 
-@router.post("/mappings/cities")
-async def create_new_mapping_for_a_location(): ...
+@router.post("/cities")
+async def create_new_mapping_for_a_city(): ...
 
-@router.post("/mappings/states")
-async def create_new_mapping_for_a_location(): ...
+@router.post("/states")
+async def create_new_mapping_for_a_state(): ...
+
+@router.put("/customers")
+async def modify_mapping_for_a_customer(): ...
+
+@router.put("/cities")
+async def modify_mapping_for_a_city(): ...
+
+@router.put("/states")
+async def modify_mapping_for_a_state(): ...

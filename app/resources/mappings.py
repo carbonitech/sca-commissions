9431d210e4c177f655f1cac4531dbf83fb9821c3
Joe Carboni <joe@carbonitech.com>
2022-08-12 15:33:48 -0400

    fixed function names

--- app/resources/mappings.py
+++ app/resources/mappings.py
@@ -33,12 +33,12 @@ async def get_all_mappings_related_to_a_specific_customer(customer_id: int):
     return {"names": name_mappings, "reps_by_branch": rep_mappings}
 
 @router.get("/customers/{customer_id}/names", tags=["customers"])
-async def get_all_mappings_related_to_a_specific_customer(customer_id: int):
+async def get_name_mappings_for_a_customer(customer_id: int):
     name_mappings = json.loads(db.get_all_customer_name_mappings(customer_id).to_json(orient="records"))
     return {"names": name_mappings}
 
 @router.get("/customers/{customer_id}/reps", tags=["customers"])
-async def get_all_mappings_related_to_a_specific_customer(customer_id: int):
+async def get_rep_mappings_for_a_customer(customer_id: int):
     rep_mappings = json.loads(db.get_rep_to_customer_full(customer_id).to_json(orient="records"))
     return {"reps_by_branch": rep_mappings}
 
@@ -85,7 +85,7 @@ async def get_all_mappings_for_city_names():
     return json.loads(db.get_all_city_name_mappings().to_json(orient="records"))
 
 @router.get("/cities/{city_id}", tags=["cities"])
-async def get_all_mappings_a_city(city_id: int):
+async def get_all_mappings_for_a_city(city_id: int):
     return json.loads(db.get_all_city_name_mappings(city_id).to_json(orient="records"))
 
 @router.post("/cities", tags=["cities"])

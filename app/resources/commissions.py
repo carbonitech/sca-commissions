f045368b22b4459e517a1a2662ef204e72c9ad4d
Joe Carboni <joe@carbonitech.com>
2022-08-16 17:28:18 -0400

    validators adjust data in pyd cls. impl post

--- app/resources/commissions.py
+++ app/resources/commissions.py
@@ -1,6 +1,6 @@
 from multiprocessing.spawn import import_main_path
 from fastapi import APIRouter, HTTPException, Form, File
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Extra
 import json
 
 from db import db_services
@@ -11,19 +11,32 @@ from entities.commission_file import CommissionFile
 from services.api_adapter import ApiAdapter
 
 db = db_services.DatabaseServices()
-db_views = db_services.TableViews()
 api = ApiAdapter()
 router = APIRouter(prefix="/commissions")
 
 class CustomCommissionData(BaseModel):
-    rep_customer_mapping_id: int
+    map_rep_customer_id: int
     inv_amt: float
     comm_amt: float
-    description: str
+
+    @validator('map_rep_customer_id')
+    def rep_customer_id_exists(cls, value):
+        exists = api.rep_customer_id_exists(value)
+        if not exists:
+            raise ValueError('Rep-to-Customer Relationship Does Not Exist')
+        return value
+
+    @validator('inv_amt')
+    def scale_up_inv_amt(cls, value):
+        return value*100
+
+    @validator('comm_amt')
+    def scale_up_comm_amt(cls, value):
+        return value*100
 
 @router.get("/", tags=['commissions'])
 async def get_commission_data():
-    return {"data": json.loads(db_views.commission_data_with_all_names().to_json(orient="records"))}
+    return {"data": json.loads(api.commission_data_with_all_names().to_json(orient="records"))}
 
 @router.post("/", tags=['commissions'])
 async def process_data_from_a_file(file: bytes = File(), reporting_month: int = Form(),
@@ -38,8 +51,12 @@ async def process_data_from_a_file(file: bytes = File(), reporting_month: int = 
         "errors":json.loads(api.get_errors(mfg_report_processor.submission_id).to_json(orient="records"))}
 
 @router.post("/{submission_id}", tags=['commissions'])
-async def add_custom_entry_to_commission_data(submission_id: int, data: CustomCommissionData):
-    raise NotImplementedError
+async def add_custom_entry_to_commission_data(submission_id: int, data: CustomCommissionData, description: str):
+    if not api.submission_exists(submission_id):
+        raise HTTPException(400, detail="report submission does not exist")
+    row_id = api.set_new_commission_data_entry(submission_id=submission_id, **data.dict())
+    return {row_id: description}
+    
 
 @router.put("/{row_id}", tags=['commissions'])
 async def modify_an_entry_in_commission_data(row_id: int, data: CustomCommissionData):

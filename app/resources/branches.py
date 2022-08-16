bb1fdbf21843233cde988a23521816e02f944dcc
Joe Carboni <joe@carbonitech.com>
2022-08-16 16:08:43 -0400

    changed name of exist check

--- app/resources/branches.py
+++ app/resources/branches.py
@@ -13,15 +13,14 @@ class Branch(BaseModel):
 @router.post("/", tags=['branches'])
 async def new_branch_by_customer_id(new_branch: Branch):
     existing_branches = api.get_customer_branches_raw(new_branch.customer_id)
-    exist_check = existing_branches[
+    not_existing_branch = existing_branches[
         (existing_branches["customer_id"] == new_branch.customer_id)
         & (existing_branches["city_id"] == new_branch.city_id)
         & (existing_branches["state_id"] == new_branch.state_id)
     ].empty
-    if exist_check:
+    if not_existing_branch:
         return api.set_new_customer_branch_raw(**new_branch.dict())
-    else:
-        raise HTTPException(status_code=400, detail="Customer Branch already exists")
+    raise HTTPException(status_code=400, detail="Customer Branch already exists")
 
 @router.delete("/{branch_id}", tags=['branches'])
 async def delete_branch_by_id(branch_id: int):

478f660f5e8e15c75757c644a74d813eaade7054
Joe Carboni <joe@carbonitech.com>
2022-08-24 12:43:25 -0400

    accepts and passes arbitrary args

--- app/error_listener.py
+++ app/error_listener.py
@@ -19,46 +19,46 @@ def error_factory(error_data: DataFrame, type_: int, submission_id: int) -> List
         result_list.append(error_obj)
     return result_list
 
-def no_customer_error(data_affected: DataFrame, submission_id: int) -> None:
+def no_customer_error(data_affected: DataFrame, submission_id: int, *args, **kwargs) -> None:
     if data_affected.empty: return
     errors = error_factory(data_affected,1,submission_id)
     db = db_services.DatabaseServices()
     for error in errors:
         db.record_error(error)
-    event.post_event("Errors Recorded", errors, submission_id)
+    event.post_event("Errors Recorded", errors, submission_id, *args, **kwargs)
 
 
-def no_city_error(data_affected: DataFrame, submission_id: int) -> None:
+def no_city_error(data_affected: DataFrame, submission_id: int, *args, **kwargs) -> None:
     if data_affected.empty: return
     errors = error_factory(data_affected,2,submission_id)
     db = db_services.DatabaseServices()
     for error in errors:
         db.record_error(error)
-    event.post_event("Errors Recorded", errors, submission_id)
+    event.post_event("Errors Recorded", errors, submission_id, *args, **kwargs)
 
-def no_state_error(data_affected: DataFrame, submission_id: int) -> None:
+def no_state_error(data_affected: DataFrame, submission_id: int, *args, **kwargs) -> None:
     if data_affected.empty: return
     errors = error_factory(data_affected,3,submission_id)
     db = db_services.DatabaseServices()
     for error in errors:
         db.record_error(error)
-    event.post_event("Errors Recorded", errors, submission_id)
+    event.post_event("Errors Recorded", errors, submission_id, *args, **kwargs)
 
-def no_branch_error(data_affected: DataFrame, submission_id: int) -> None:
+def no_branch_error(data_affected: DataFrame, submission_id: int, *args, **kwargs) -> None:
     if data_affected.empty: return
     errors = error_factory(data_affected,4,submission_id)
     db = db_services.DatabaseServices()
     for error in errors:
         db.record_error(error)
-    event.post_event("Errors Recorded", errors, submission_id)
+    event.post_event("Errors Recorded", errors, submission_id, *args, **kwargs)
 
-def no_rep_assigned_error(data_affected: DataFrame, submission_id: int) -> None:
+def no_rep_assigned_error(data_affected: DataFrame, submission_id: int, *args, **kwargs) -> None:
     if data_affected.empty: return
     errors = error_factory(data_affected,5,submission_id)
     db = db_services.DatabaseServices()
     for error in errors:
         db.record_error(error)
-    event.post_event("Errors Recorded", errors, submission_id)
+    event.post_event("Errors Recorded", errors, submission_id, *args, **kwargs)
 
 
 def setup_error_event_handlers():

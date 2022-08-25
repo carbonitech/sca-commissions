3ff3cd829d46ea4b2739eb2ce572071fc6ee1c89
Joe Carboni <joe@carbonitech.com>
2022-08-25 07:08:58 -0400

    steps properly recorded for error reintegration

--- app/process_step_listener.py
+++ app/process_step_listener.py
@@ -4,14 +4,11 @@ from app import event
 from entities import error
 from entities.processing_step import ProcessingStep
 from db.db_services import DatabaseServices
+from entities.error import ErrorType
 
 def handle_errors_recorded(error_list: List[error.Error], submission_id: int, *args, **kwargs) -> None:
     num_rows = len(error_list)
-    row_index_list = [err.row_index for err in error_list]
-
-    desc_str = f"{num_rows} rows failed to match to standard references due to {error_list[0].reason.name}.\n" \
-                f"Rows indices: {row_index_list}"
-
+    desc_str = f"{num_rows} rows failed to match to standard references due to {error_list[0].reason.name}."
     if start_step := kwargs.get("start_step"):
         ProcessingStep.set_total_step_num(start_step)
         
@@ -26,9 +23,8 @@ def handle_rows_removed(data_removed: DataFrame, submission_id: int, *args, **kw
     sum_inv_removed = data_removed.loc[:,"inv_amt"].sum()/100
     sum_comm_removed = data_removed.loc[:,"comm_amt"].sum()/100
 
-    desc_str = f"{num_rows} rows removed from commission data.\n"\
-            f"Rows removed: {row_index_list}\n" \
-            f"Total Inv_amt reduced by ${sum_inv_removed:,.2f} and Comm_amt reduced by ${sum_comm_removed:,.2f}"
+    desc_str = f"{num_rows} rows dropped."\
+            f" Total Inv_amt excluded: ${sum_inv_removed:,.2f} and Comm_amt excluded: ${sum_comm_removed:,.2f}"
 
     if start_step := kwargs.get("start_step"):
         ProcessingStep.set_total_step_num(start_step)
@@ -47,12 +43,12 @@ def handle_data_formatting(msg: str, submission_id: int, *args, **kwargs) -> Non
 
 def handle_comm_data_recorded(data: DataFrame, submission_id: int, *args, **kwargs) -> None:
     num_rows = len(data)
-    sum_inv_removed = data.loc[:,"inv_amt"].sum()/100
-    sum_comm_removed = data.loc[:,"comm_amt"].sum()/100
+    sum_inv = data.loc[:,"inv_amt"].sum()/100
+    sum_comm = data.loc[:,"comm_amt"].sum()/100
 
-    desc_str = f"{num_rows} rows written to the database.\n"\
-            f"Total Inv_amt = ${sum_inv_removed:,.2f}\n"\
-            f"Comm_amt = ${sum_comm_removed:,.2f}"
+    desc_str = f"{num_rows} rows written to the final commission data table."\
+            f" Total Sales added: ${sum_inv:,.2f}"\
+            f" Total Commissions added = ${sum_comm:,.2f}"
     
     if start_step := kwargs.get("start_step"):
         ProcessingStep.set_total_step_num(start_step)
@@ -61,8 +57,11 @@ def handle_comm_data_recorded(data: DataFrame, submission_id: int, *args, **kwar
     db.record_processing_step(ProcessingStep(desc_str,submission_id))
 
 
-def handle_mapping_table_updated(data) -> None:
-    ...
+def handle_mapping_table_updated(data, submission_id, *args, **kwargs) -> None:
+    if start_step := kwargs.get("start_step"):
+        ProcessingStep.set_total_step_num(start_step)
+    db = DatabaseServices()
+    db.record_processing_step(ProcessingStep(data,submission_id))
 
 
 def setup_processing_step_handlers():
@@ -70,3 +69,6 @@ def setup_processing_step_handlers():
     event.subscribe("Rows Removed", handle_rows_removed)
     event.subscribe("Formatting", handle_data_formatting)
     event.subscribe("Data Recorded", handle_comm_data_recorded)
+    event.subscribe("Reprocessing", handle_mapping_table_updated)
+
+

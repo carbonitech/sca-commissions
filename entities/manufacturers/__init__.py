6878a3789c25068c3a667e5096f5a0c30ec0e787
Joe Carboni <joe@carbonitech.com>
2022-09-02 17:34:02 -0400

    skips process if no manfs in db

--- entities/manufacturers/__init__.py
+++ entities/manufacturers/__init__.py
@@ -12,15 +12,23 @@ all_manufacturers_in_dir = [
     ]
 
 MFG_PREPROCESSORS = {}
+if not all_manufacturers_in_db:
+    pass # don't do anything else if the db manufacturers table is empty
+else:
 
-import_prefix = "entities.manufacturers."
-for id_num, manufacturer_in_db in all_manufacturers_in_db.items():
-    try:
-        imported_obj = import_module(import_prefix + manufacturer_in_db)
-    except ModuleNotFoundError:
-        pass
-    else:
-        MFG_PREPROCESSORS[id_num] = imported_obj.PreProcessor
+    import_prefix = "entities.manufacturers."
+    for id_num, manufacturer_in_db in all_manufacturers_in_db.items():
+        try:
+            imported_obj = import_module(import_prefix + manufacturer_in_db)
+        except ModuleNotFoundError:
+            pass
+        else:
+            MFG_PREPROCESSORS[id_num] = imported_obj.PreProcessor
+
+    del import_prefix
+    del id_num
+    del manufacturer_in_db
+    del imported_obj
 
 del os
 del import_module
@@ -28,7 +36,3 @@ del DatabaseServices
 del db
 del all_manufacturers_in_db
 del all_manufacturers_in_dir
-del import_prefix
-del id_num
-del manufacturer_in_db
-del imported_obj

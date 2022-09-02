734e46640628cf5a97dccf3037c303e08e34fe93
Joe Carboni <joe@carbonitech.com>
2022-09-02 17:02:44 -0400

    added sqlalchemy Base.create_all at top of main

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from db.db_services import DatabaseServices
 from db.models import Base
 
 
+Base.metadata.create_all(DatabaseServices.engine)
 app = FastAPI()
 token_auth_scheme = HTTPBearer()
 AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')

74cd6d9afc5a9587d88ea977caa6a50657a9b1ca
Joe Carboni <joe@carbonitech.com>
2022-09-06 07:06:09 -0400

    moved mappings under same module, change ep syntax

--- app/resources/cities.py
+++ app/resources/cities.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Form
 from pydantic import BaseModel, validator
 import json
 from services.api_adapter import ApiAdapter
@@ -14,6 +14,7 @@ class City(BaseModel):
     @validator('name')
     def name_to_uppercase(cls, value: str):
         return value.strip().upper()
+
 
 @router.get("/", tags=["cities"])
 async def get_all_cities():
@@ -40,7 +41,7 @@ async def add_a_city(city:City = Depends(City.as_form)):
     raise HTTPException(400, detail=f"City exists with id {existing_id}")
 
 @router.put("/{city_id}", tags=["cities"])
-async def modify_a_city(city_id: int, updated_city: City):
+async def modify_a_city(city_id: int, updated_city: City = Depends(City.as_form)):
     existing_city = api.get_cities(city_id)
     if existing_city.empty:
         raise HTTPException(400, detail=f"City with id {city_id} does not exist")
@@ -50,3 +51,28 @@ async def modify_a_city(city_id: int, updated_city: City):
 async def delete_a_city(city_id: int):
     # soft delete
     api.delete_city_by_id(city_id)
+
+
+@router.get("/{city_id}/mappings", tags=["cities"])
+async def get_all_mappings_for_a_city(city_id: int):
+    return json.loads(api.get_all_city_name_mappings(city_id).to_json(orient="records"))
+
+@router.post("/{city_id}/mappings", tags=["cities"])
+async def create_new_mapping_for_a_city(city_id: int, new_mapping: str = Form()):
+    current_city = api.get_cities(city_id)
+    if current_city.empty:
+        raise HTTPException(400, detail="City name does not exist")
+    value = {"city_id": city_id, "recorded_name": new_mapping}
+    api.set_city_name_mapping(**value)
+
+@router.put("/{city_id}/mappings/{mapping_id}", tags=["cities"])
+async def modify_mapping_for_a_city(city_id: int, mapping_id: int, modified_mapping: str = Form()):
+    city = api.get_cities(city_id)
+    if city.empty:
+        raise HTTPException(400, detail=f"City with id {city_id} does not exist")
+    all_mappings = api.get_all_city_name_mappings(city_id)
+    mapping_ids = all_mappings.loc[:,"mapping_id"]
+    if mapping_id not in mapping_ids.values:
+        raise HTTPException(400, detail=f"this mapping does not exist for city name {city.name.squeeze()}")
+    value = {"city_id": city_id, "recorded_name": modified_mapping}
+    api.modify_city_name_mapping(mapping_id,**value)

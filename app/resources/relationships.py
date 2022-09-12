c131de951af9a55c108a49bff6b927e9af8bdc98
Joe Carboni <joe@carbonitech.com>
2022-09-12 13:35:21 -0400

    placeholder for self relationship links

--- app/resources/relationships.py
+++ app/resources/relationships.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from services.api_adapter import ApiAdapter
 
@@ -12,6 +12,10 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/{primary}/{id_}/{secondary}")
+@router.get("/{primary}/{id_}/{secondary}", tags=["relationships"])
 def get_related_handler(primary:str, id_:int, secondary:str, db: Session=Depends(get_db)):
     return api.get_related(db,primary,id_,secondary)
+
+@router.get("/{primary}/{id_}/relationships/{secondary}", tags=["relationships"])
+def get_self_relationship_handler(primary:str, id_:int, secondary:str, db: Session=Depends(get_db)):
+    raise HTTPException(status_code=501, detail="self relationship links not implemented")

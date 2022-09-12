d65c0909c17267607e9116373bb27762e64c85c1
Joe Carboni <joe@carbonitech.com>
2022-09-12 13:27:27 -0400

    added bidir relationships to models

--- db/models.py
+++ db/models.py
@@ -13,8 +13,15 @@ class City(Base):
     id = Column(Integer,primary_key=True)
     name = Column(String)
     deleted = Column(DateTime)
-    branch_cities = relationship("CustomerBranch")
-    map_names = relationship("MapCityName")
+    branch_cities = relationship("CustomerBranch", back_populates="city_name")
+    map_names = relationship("MapCityName", back_populates="city_name")
+
+class MapCityName(Base):
+    __tablename__ = 'map_city_names'
+    id = Column(Integer,primary_key=True)
+    recorded_name = Column(String)
+    city_id = Column(Integer, ForeignKey("cities.id"))
+    city_name = relationship("City", back_populates="map_names")
 
 
 class State(Base):
@@ -22,8 +29,15 @@ class State(Base):
     id = Column(Integer,primary_key=True)
     name = Column(String)
     deleted = Column(DateTime)
-    branch_states = relationship("CustomerBranch")
-    map_names = relationship("MapStateName")
+    branch_states = relationship("CustomerBranch", back_populates="state_name")
+    map_names = relationship("MapStateName", back_populates="state_name")
+
+class MapStateName(Base):
+    __tablename__ = 'map_state_names'
+    id = Column(Integer,primary_key=True)
+    recorded_name = Column(String)
+    state_id = Column(Integer, ForeignKey("states.id"))
+    state_name = relationship("State", back_populates="map_names")
 
 
 class Customer(Base):
@@ -31,30 +45,37 @@ class Customer(Base):
     id = Column(Integer,primary_key=True)
     name = Column(String)
     deleted = Column(DateTime)
-    branches = relationship("CustomerBranch", back_populates="customer")
+    customer_branches = relationship("CustomerBranch", back_populates="customer")
     map_names = relationship("MapCustomerName", back_populates="customer")
 
+class MapCustomerName(Base):
+    __tablename__ = 'map_customer_name'
+    id = Column(Integer,primary_key=True)
+    recorded_name = Column(String)
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    customer = relationship("Customer", back_populates="map_names")
 
 class CustomerBranch(Base):
     __tablename__ = 'customer_branches'
-    __jsonapi_type_override__ = 'branches'
     id = Column(Integer,primary_key=True)
     customer_id = Column(Integer, ForeignKey("customers.id"))
     city_id = Column(Integer, ForeignKey("cities.id"))
     state_id = Column(Integer, ForeignKey("states.id"))
     deleted = Column(DateTime)
-    rep = relationship("MapRepToCustomer")
-    customer = relationship("Customer", back_populates="branches")
+    rep = relationship("MapRepToCustomer", back_populates="branch")
+    customer = relationship("Customer", back_populates="customer_branches")
+    city_name = relationship("City", back_populates="branch_cities")
+    state_name = relationship("State", back_populates="branch_states")
 
-
-class ManufacturersReport(Base):
-    __tablename__ = 'manufacturers_reports'
+class MapRepToCustomer(Base):
+    __tablename__ = 'map_reps_customers'
     id = Column(Integer,primary_key=True)
-    manufacturer_id = Column(Integer, ForeignKey("manufacturers.id"))
-    report_name = Column(String)
-    yearly_frequency = Column(Integer)
-    POS_report = Column(Boolean)
-    deleted = Column(DateTime)
+    rep_id = Column(Integer, ForeignKey("representatives.id"))
+    customer_branch_id = Column(Integer, ForeignKey("customer_branches.id"))
+    orphaned = Column(DateTime)
+    commission_data = relationship("FinalCommissionDataDTO", back_populates="rep_customer")
+    rep_name = relationship("Representative", back_populates="branches")
+    branch = relationship("CustomerBranch", back_populates="rep")
 
 
 class Representative(Base):
@@ -65,37 +86,17 @@ class Representative(Base):
     initials = Column(String)
     date_joined = Column(DateTime)
     deleted = Column(DateTime)
-    branches = relationship("MapRepToCustomer")
+    branches = relationship("MapRepToCustomer", back_populates="rep_name")
 
-class MapCustomerName(Base):
-    __tablename__ = 'map_customer_name'
+
+class ManufacturersReport(Base):
+    __tablename__ = 'manufacturers_reports'
     id = Column(Integer,primary_key=True)
-    recorded_name = Column(String)
-    customer_id = Column(Integer, ForeignKey("customers.id"))
-    customer = relationship("Customer", back_populates="map_names")
-
-
-class MapCityName(Base):
-    __tablename__ = 'map_city_names'
-    id = Column(Integer,primary_key=True)
-    recorded_name = Column(String)
-    city_id = Column(Integer, ForeignKey("cities.id"))
-
-
-class MapStateName(Base):
-    __tablename__ = 'map_state_names'
-    id = Column(Integer,primary_key=True)
-    recorded_name = Column(String)
-    state_id = Column(Integer, ForeignKey("states.id"))
-
-
-class MapRepToCustomer(Base):
-    __tablename__ = 'map_reps_customers'
-    id = Column(Integer,primary_key=True)
-    rep_id = Column(Integer, ForeignKey("representatives.id"))
-    customer_branch_id = Column(Integer, ForeignKey("customer_branches.id"))
-    orphaned = Column(DateTime)
-    commission_data = relationship("FinalCommissionDataDTO")
+    manufacturer_id = Column(Integer, ForeignKey("manufacturers.id"))
+    report_name = Column(String)
+    yearly_frequency = Column(Integer)
+    POS_report = Column(Boolean)
+    deleted = Column(DateTime)
 
 ## Entity DTOs
 class ManufacturerDTO(Base):
@@ -143,6 +144,7 @@ class FinalCommissionDataDTO(Base):
     map_rep_customer_id = Column(Integer, ForeignKey("map_reps_customers.id"))
     inv_amt = Column(Float)
     comm_amt = Column(Float)
+    rep_customer = relationship("MapRepToCustomer", back_populates="commission_data")
 
 setattr(Base,"_decl_class_registry",Base.registry._class_registry) # because JSONAPI's constructor is broken
 serializer = JSONAPI(Base)

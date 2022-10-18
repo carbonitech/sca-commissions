98840695a29966ab148ea6860f2f7aada3884431
carbonitech <joe@carbonitech.com>
2022-10-18 14:15:28 -0400

    test patch changes target table and mapping table

--- tests/test_patch_requests.py
+++ tests/test_patch_requests.py
@@ -8,7 +8,7 @@ from tests.pytest_fixtures import set_overrides, clear_overrides, database
 test_client = TestClient(app)
 
 
-def test_customers_patch(database: database):
+def test_customers_patch_returns_jsonapi_obj(database: database):
     set_overrides()
     test_id = randint(1,50)
     request_body = {
@@ -77,3 +77,47 @@ def test_customers_patch_bad_body(database: database):
                     case {"status": 422, "detail": "field required", "field": field}:
                         no_body[i] = True
     assert all(no_body)
+
+def test_customers_patch_makes_change_in_target(database: database):
+    set_overrides()
+    test_id = randint(1,50)
+    request_body = {
+        "data": {
+            "type": "customers",
+            "id": test_id,
+            "attributes": {
+                "name": "NEW AWESOME BUSINESS, LLC"
+            }
+        }
+    }
+    test_client.patch(f"/customers/{test_id}", data=dumps(request_body))
+    hit = False
+    try:
+        new_name = database.execute("SELECT name FROM customers WHERE id = :id", {"id": test_id}).scalar()
+        hit = True if new_name == "NEW AWESOME BUSINESS, LLC" else False
+    finally:
+        database.close() # need this to prevent the test from hanging indefinitely
+    assert hit, f"name in database with id: {str(test_id)} is {str(new_name)}. Expected NEW AWESOME BUSINESS, LLC"
+
+
+def test_customers_patch_records_new_name_in_mapping(database: database):
+    set_overrides()
+    test_id = randint(1,50)
+    request_body = {
+        "data": {
+            "type": "customers",
+            "id": test_id,
+            "attributes": {
+                "name": "NEW AWESOME BUSINESS, LLC"
+            }
+        }
+    }
+    test_client.patch(f"/customers/{test_id}", data=dumps(request_body))
+    hit = False
+    try:
+        new_mapping_name = database.execute("SELECT recorded_name FROM map_customer_name WHERE customer_id = :id ORDER BY id DESC LIMIT 1", {"id": test_id}).scalar()  
+        hit = True if new_mapping_name == "NEW AWESOME BUSINESS, LLC" else False
+    finally:
+        database.close() # need this to prevent the test from hanging indefinitely
+
+    assert hit, f"last mapping name in database with customer_id: {str(test_id)} is {str(new_mapping_name)}. Expected NEW AWESOME BUSINESS, LLC."

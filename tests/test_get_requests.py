695c47a855d0b886d6d6e1d4a7a557b4a4926df1
carbonitech <joe@carbonitech.com>
2022-10-21 11:53:29 -0400

    improve tests

--- tests/test_get_requests.py
+++ tests/test_get_requests.py
@@ -1,11 +1,23 @@
-from fastapi.testclient import TestClient
 from app.main import app
+from fastapi.testclient import TestClient
 from tests.pytest_fixtures import set_overrides, clear_overrides, database
 
 test_client = TestClient(app)
 
 def test_jsonapi_object_response_from_all_top_level(database: database):
-    resources = ["customers","cities","states","commission-data","manufacturers","representatives","submissions"]
+    openapi: dict[str,dict|str] = test_client.get("/openapi.json").json()
+    resources = set(
+        [
+            path.split("/")[1] 
+            for path in openapi["paths"].keys() 
+            if not (
+                path.startswith("/{") 
+                or path == '/resetdb' 
+                or path == "/download" 
+                or path == "/"
+                or path.startswith("/branches"))
+        ]
+    )
     for resource in resources:
         set_overrides()
         response = test_client.get(f"/{resource}")
@@ -32,16 +44,30 @@ def test_get_customers_with_filter_parameter(database: database):
 
 def test_jsonapi_param_handling(database: database):
     set_overrides()
-    filter_param = "filter[name] = MINGLEDORFF"
-    page_params = "page[size]=5&page[number]=1"
+    resource = "customers"
+    customer_name = "AAA SUPPLY"
+    page_size = 5
+    field = "name"
     headers_content_type = {"Content-Type": "application/vnd.api+json"}
 
-    response = test_client.get(f"/customers?{page_params}", headers=headers_content_type)
+    filter_param = f"filter[name]={customer_name}"
+    page_params = f"page[size]={str(page_size)}&page[number]=1"
+    fields_param = f"fields[{resource}]={field}"
+
+    response = test_client.get(f"/{resource}?{page_params}", headers=headers_content_type)
     assert len(response.json()["data"]) == 5
 
-    response = test_client.get(f"/customers?{filter_param}", headers=headers_content_type)
-    # assert len(response.json()["data"]) == 1, f"{response.json()}"
-    assert response.json()["data"][0]["attributes"]["name"].startswith("MINGLEDORFF")
+    response = test_client.get(f"/{resource}?{filter_param}", headers=headers_content_type)
+    assert len(response.json()["data"]) == 1
+    assert response.json()["data"][0]["attributes"]["name"] == customer_name
+
+    response = test_client.get(f"/{resource}?{filter_param}&{page_params}", headers=headers_content_type)
+    assert len(response.json()["data"]) == 1
+    assert response.json()["data"][0]["attributes"]["name"] == customer_name
+
+    response = test_client.get(f"/{resource}?{fields_param}", headers=headers_content_type)
+    attr_list = list(response.json()["data"][0]["attributes"].keys())
+    assert len(attr_list) == 1
+    assert attr_list[0] == field
 
 
-    

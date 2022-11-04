d35c7d354d516c55a08e6df739d7378ee3c7c958
carbonitech <joe@carbonitech.com>
2022-11-04 11:58:27 -0400

    rm print statements

--- entities/manufacturers/hardcast.py
+++ entities/manufacturers/hardcast.py
@@ -24,8 +24,6 @@ class PreProcessor(AbstractPreProcessor):
         inv_col: str = "Sales Base for Comm"
         comm_col: str = "Comm"
 
-        print("1")
-        print(data)
         drop_col = "Sales Group"
         data = data.drop(columns=drop_col)
         events.append(("Formatting",f"dropped column {drop_col}",self.submission_id))
@@ -33,14 +31,10 @@ class PreProcessor(AbstractPreProcessor):
         events.append(("Formatting","removed all rows with a star (*) in the Comm Rate column",self.submission_id))
         data = data.fillna(method="ffill")
         events.append(("Formatting",f"converted to a flat table by copying customer info (name, city, state) into blank rows",self.submission_id))
-        print("2")
-        print(data)
         result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
         result.loc[:,inv_col] = result[inv_col]*100
         result.loc[:,comm_col] = result[comm_col]*100
         result.columns = self.result_columns # local result.cols are same length and position as self.result_columns
-        print("3")
-        print(result)
         return PreProcessedData(result,events)
 
 

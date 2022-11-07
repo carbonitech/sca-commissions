88762ef23db05692e798030c591219f0307d4ced
carbonitech <joe@carbonitech.com>
2022-11-07 17:30:41 -0500

    rm import

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -9,5 +9,4 @@ from app.resources.cities import router as cities
 from app.resources.states import router as states
 from app.resources.relationships import router as relationships
 from app.resources.download import router as downloads
-from app.resources.resetdb import router as resetdb
 from app.resources.reports import router as reports

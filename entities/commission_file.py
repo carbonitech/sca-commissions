82c7e9610abaf385adfe0ef9ff11d9c504cc8ec1
carbonitech <joe@carbonitech.com>
2022-11-09 12:02:52 -0500

    read visible sheets only

--- entities/commission_file.py
+++ entities/commission_file.py
@@ -7,9 +7,15 @@ class CommissionFile:
     file_data: bytes|BytesIO
 
     def to_df(self, combine_sheets=False) -> pd.DataFrame:
-        if combine_sheets:
-            with pd.ExcelFile(self.file_data) as excel_file:
-                excel_file: pd.ExcelFile
-                data = [excel_file.parse(sheet) for sheet in excel_file.sheet_names]
+        """
+        read only visible sheets in the excel file
+        if combine_sheets is True, attempt to UNION all visible sheets
+        """
+        with pd.ExcelFile(self.file_data, engine="openpyxl") as excel_file:
+            excel_file: pd.ExcelFile
+            visible_sheets = [sheet.title for sheet in excel_file.book.worksheets if sheet.sheet_state == "visible"]
+            if combine_sheets:
+                data = [excel_file.parse(sheet) for sheet in visible_sheets]
                 return pd.concat(data, ignore_index=True)
-        return pd.read_excel(self.file_data)
+            
+            return pd.read_excel(self.file_data, sheet_name=visible_sheets[0])

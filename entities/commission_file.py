9698a6b2c42a21c74669428db01603bec0bb34c3
carbonitech <joe@carbonitech.com>
2022-11-09 10:47:32 -0500

    updated file_data typing

--- entities/commission_file.py
+++ entities/commission_file.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 import pandas as pd
+from io import BytesIO
 
 @dataclass
 class CommissionFile:
-    file_data: bytes
+    file_data: bytes|BytesIO
 
     def to_df(self, combine_sheets=False) -> pd.DataFrame:
         if combine_sheets:

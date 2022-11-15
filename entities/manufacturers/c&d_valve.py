7cad3eea73137b924fc327b8d78926d4ea79e285
carbonitech <joe@carbonitech.com>
2022-11-15 10:39:32 -0500

    completed processing and added processing_steps

--- entities/manufacturers/c&d_valve.py
+++ entities/manufacturers/c&d_valve.py
@@ -26,27 +26,35 @@ class PreProcessor(AbstractPreProcessor):
         invoice_num_col: int = 1
         address_col: int = 9
 
-        summary_file: pd.DataFrame = pd.read_excel(kwargs.get("additional_file_1"))
+        summary_file: pd.DataFrame = pd.read_excel(kwargs.get("additional_file_1")).dropna()
         invoice_number_col: str = "Invoice Number"
         comm_col: int = -1
 
         # inoice numbers are in their own column and share the same index as customer info and sales figure
         value_indecies = data.iloc[:,invoice_num_col].dropna().index.to_list()[1:] # 0th value is the column header
         result = data.loc[value_indecies]
+        result.iloc[:,invoice_num_col] = result.iloc[:,invoice_num_col].astype(int)
+        events.append(("Formatting","filtered for rows with an invoice number in the second column and converted from text to numbers",self.submission_id))
         # address is combined and needs to be split into city and state
         result[[city_name_col,state_name_col]] = result.iloc[:,address_col].str.split(", ", expand=True)
         result.loc[:, state_name_col] = result[state_name_col].str.slice(0,2)
+        events.append(("Formatting",f"split the address in column {address_col+1} into city and state columns",self.submission_id))
         # make a new table with just invoice number, customer, sales, city, and state
         result = pd.concat([result.iloc[:,[1,customer_name_col,inv_col]], result.loc[:,[city_name_col,state_name_col]]], axis=1)
         # reorder columns so invoice amount is at the end
         result = result.iloc[:,[0,1,3,4,2]]
         result.columns = ["invoice", "customer", "city", "state", "inv_amt"]
+        result.loc[:, "inv_amt"] = result["inv_amt"]*100
         result = result.groupby(result.columns.tolist()[:4]).sum().reset_index()
+        events.append(("Formatting",f"grouped data by invoice number, customer, city, and state. Index changed.",self.submission_id))
+        
         # merge with the summary_file to fill total commission values by invoice number!
         summary_file = pd.concat([summary_file.loc[:,invoice_number_col], summary_file.iloc[:,comm_col]], axis=1)
         summary_file.columns = ["invoice_summary","total_comm"]
+        summary_file["invoice_summary"] = summary_file["invoice_summary"].astype(int)
         merged = result.merge(summary_file, how="left", left_on="invoice", right_on="invoice_summary")
-        result.loc[:,"comm_amt"] = merged["total_comm"]
+        result.loc[:,"comm_amt"] = merged["total_comm"]*100
+        events.append(("Formatting",f"Merged Commission Amounts by invoice number using the reference file",self.submission_id))
         # drop invoice number column
         result = result.drop(columns="invoice")
         # standardize
@@ -55,5 +63,20 @@ class PreProcessor(AbstractPreProcessor):
             result.iloc[:, col] = result.iloc[:, col].str.upper()
             result.iloc[:, col] = result.iloc[:, col].str.strip()
 
+        # without invoice column now, group by branch
+        result = result.groupby(result.columns.tolist()[:3]).sum().reset_index()
+        events.append(("Formatting",f"Dropped the Invoice number column and grouped sales again by Customer, City, and State. Index changed.",self.submission_id))
+
         return PreProcessedData(result, events)
+
+
+    def preprocess(self, **kwargs) -> PreProcessedData:
+        method_by_name = {
+            "standard": self._standard_report_preprocessing,
+        }
+        preprocess_method = method_by_name.get(self.report_name, None)
+        if preprocess_method:
+            return preprocess_method(self.file.to_df(), **kwargs)
+        else:
+            return
 

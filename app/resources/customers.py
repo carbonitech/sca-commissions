cfeedd59336f7f822af78454188a213a1a23d251
carbonitech <joe@carbonitech.com>
2022-11-28 07:12:38 -0500

    endpoints use user obj to target records

--- app/resources/customers.py
+++ app/resources/customers.py
@@ -9,21 +9,27 @@ router = APIRouter(prefix="/customers", route_class=JSONAPIRoute)
 @router.get("", tags=["customers"])
 async def all_customers(query: Query=Depends(), db: Session=Depends(get_db), user: User=Depends(get_user)):
     jsonapi_query = convert_to_jsonapi(query)
-    return api.get_many_customers_jsonapi(db,jsonapi_query)
+    return api.get_many_customers_jsonapi(db,jsonapi_query, user)
 
 @router.get("/{customer_id}", tags=["customers"])
-async def customer_by_id(customer_id: int, query: Query=Depends(), db: Session=Depends(get_db)):
+async def customer_by_id(customer_id: int, query: Query=Depends(), db: Session=Depends(get_db), user: User=Depends(get_user)):
     jsonapi_query = convert_to_jsonapi(query)
-    return api.get_customer_jsonapi(db, customer_id, jsonapi_query)
+    return api.get_customer_jsonapi(db, customer_id, jsonapi_query, user)
    
 @router.patch("/{customer_id}", tags=["customers"])
-async def modify_customer(customer_id: int, customer: CustomerModificationRequest, db: Session=Depends(get_db)):
-    return api.modify_customer_jsonapi(db, customer_id, customer.dict())
+async def modify_customer(customer_id: int,
+        customer: CustomerModificationRequest,
+        db: Session=Depends(get_db),
+        user: User=Depends(get_user)):
+        
+    return api.modify_customer_jsonapi(db, customer_id, customer.dict(), user)
 
 @router.post("", tags=["customers"])
-async def new_customer():
+async def new_customer(db: Session=Depends(get_db), user: User=Depends(get_user)):
+    user.id(db=db)
     ...
 
 @router.delete("/{customer_id}", tags=["customers"])
-async def delete_customer(customer_id: int):
+async def delete_customer(customer_id: int, db: Session=Depends(get_db), user: User=Depends(get_user)):
+    user.id(db=db)
     ...

52c1b8e06c624c408cc418e9d7b2e3df7d857d2d
carboni-joseph <joe@carbonitech.com>
2022-12-13 16:01:18 -0500

    add PyPDF2 extraction strategy

--- entities/commission_file.py
+++ entities/commission_file.py
@@ -1,19 +1,32 @@
 from dataclasses import dataclass
 import pandas as pd
 import tabula
+from PyPDF2 import PdfFileReader
 from io import BytesIO
 
 @dataclass
 class CommissionFile:
     file_data: bytes|BytesIO
 
-    def to_df(self, combine_sheets=False, from_pdf=False) -> pd.DataFrame:
+    def to_df(self, combine_sheets=False, pdf: str=None) -> pd.DataFrame:
         """
         read only visible sheets in the excel file
         if combine_sheets is True, attempt to UNION all visible sheets
+
+        For PDF files, two strategies are available
+            "text": raw text dumped into a Panadas Series. Lines split by newline/return characted 
+            "table": if the data is formatted as a table in the sheet, extract that table as-is.
         """
-        if from_pdf:
-            return tabula.read_pdf(BytesIO(self.file_data), pages="all")[0]
+        if strategy := pdf:
+            if strategy.lower() == "text":
+                all_text = ""
+                for page in PdfFileReader(BytesIO(self.file_data)).pages:
+                    all_text += page.extract_text()
+                text_list = all_text.splitlines()
+                text_list_compact = [line.strip() for line in text_list if line]
+                return pd.Series(text_list_compact)
+            elif strategy.lower() == "table":
+                return tabula.read_pdf(BytesIO(self.file_data), pages="all")[0]
 
         try:
             with pd.ExcelFile(self.file_data, engine="openpyxl") as excel_file:

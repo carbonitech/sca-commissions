ed1c686384d85899cf2b4c53102d6a39482e09c4
carboni-joseph <joe@carbonitech.com>
2022-12-14 13:27:28 -0500

    support ambro

---
+++ entities/manufacturers/ambro_controls.py
@@ -0,0 +1,42 @@
+"""
+Manufacturer report preprocessing definition
+for Ambro Controls
+"""
+import pandas as pd
+from entities.commission_data import PreProcessedData
+from entities.preprocessor import AbstractPreProcessor
+
+class PreProcessor(AbstractPreProcessor):
+
+    def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+        """processes the Ambro Controls standard report"""
+
+        events = []
+        customer_name_col: str = "Customer Name"
+        city_name_col: str = "Ship to City"
+        state_name_col: str = "State"
+        inv_col: str = "Amount"
+        comm_col: str = "Commission Payable"
+
+        data = data.rename(columns=lambda col: col.strip())
+        data = data.dropna(subset=data.columns.to_list()[0])
+        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
+        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
+        result.loc[:,inv_col] = result[inv_col]*100
+        result.loc[:,comm_col] = result[comm_col]*100
+        for col in [customer_name_col,city_name_col,state_name_col]:
+            result.loc[:, col] = result[col].str.upper()
+            result.loc[:, col] = result[col].str.strip()
+        result.columns = self.result_columns
+        return PreProcessedData(result,events)
+
+
+    def preprocess(self, **kwargs) -> PreProcessedData:
+        method_by_name = {
+            "standard": self._standard_report_preprocessing,
+        }
+        preprocess_method = method_by_name.get(self.report_name, None)
+        if preprocess_method:
+            return preprocess_method(self.file.to_df(), **kwargs)
+        else:
+            return

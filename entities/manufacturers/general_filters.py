29f9365ace0aa3d4325762ffc333f9a11a4eb1b3
carboni-joseph <joe@carbonitech.com>
2022-12-16 13:06:51 -0500

    support general filters

---
+++ entities/manufacturers/general_filters.py
@@ -0,0 +1,93 @@
+"""
+Manufacturer report preprocessing definition
+for General Filters
+"""
+import re
+import pandas as pd
+from entities.commission_data import PreProcessedData
+from entities.preprocessor import AbstractPreProcessor
+
+class PreProcessor(AbstractPreProcessor):
+
+    def _standard_report_preprocessing(self, data: pd.Series, **kwargs) -> PreProcessedData:
+        """processes the standard General Filters file
+        
+            this report comes in as a PDF and looks more like a sales report
+            heavy amount of parsing required to get to the data, which comes in as one long series
+        """
+
+        events = []
+        customer_boundary_re: str = r"\d{4} CUST: SHIP-TO"
+        page_num_re: str = r"^(Page \d of \d)"
+        offset: int = -1
+        customer_name_sales_comm_re: str = r"([0-9^.,-]+)\s+[0-9^.,-]+\sTOTAL SHIP-TO:\s(?:DEFAULT|[0-9]+)?\s?(.+?)\s+([0-9^.,-]+)"
+        # group 1 (comm_amt), 2(customer),    ^^^^^^^^^^                                                       ^^^     ^^^^^^^^^^
+        #       3(inv_amt)
+        city_state_re: str = r"^\d{4}\sCUST:\sSHIP-TO:\s(.+)"
+        # city state combined without a space            ^^
+
+        data = data[1:]
+        data = data.loc[
+            ~data.isin(data[:4])
+            & ~data.str.contains(page_num_re)
+        ].reset_index(drop=True)
+        customer_boundaries: list[int] = [
+            index+offset 
+            for index
+            in data.loc[data.str.contains(customer_boundary_re)].index.tolist()
+        ]
+        events.append(("Formatting",f"used boundary value {customer_boundary_re} to isolate customers",self.submission_id))
+        compiled_data = {
+            "customer": [],
+            "city": [],
+            "state": [],
+            "inv_amt": [],
+            "comm_amt": []
+        }
+        for i, index in enumerate(customer_boundaries):
+            if i == len(customer_boundaries)-1:
+                # last customer, go to the end of the dataset, except grand totals
+                subseries = data.iloc[index:-1]
+            else:
+                subseries = data.iloc[index:customer_boundaries[i+1]]
+
+            print(subseries)
+            city_state = re.match(city_state_re, subseries.iloc[1]).group(1)
+            city, state = city_state[:-2], city_state[-2:] # split city from 2-letter state
+
+            customer_inv_comm: list[str] = subseries.str.extract(customer_name_sales_comm_re).dropna().values[0]
+            customer = customer_inv_comm[1]
+            inv_amt = float(customer_inv_comm[2].replace(",",""))
+            comm_amt = float(customer_inv_comm[0].replace(",",""))
+
+            compiled_data["customer"].append(customer)
+            compiled_data["city"].append(city)
+            compiled_data["state"].append(state) 
+            compiled_data["inv_amt"].append(inv_amt)
+            compiled_data["comm_amt"].append(comm_amt)
+
+        result = pd.DataFrame(compiled_data)
+        events.append(("Formatting",f"extracted customer, city, state, and sales amount and formatted into a table",self.submission_id))
+
+        result["inv_amt"] = result["inv_amt"]*100
+        result["comm_amt"] = result["comm_amt"]*100
+        for col in ["customer","city","state"]:
+            result.loc[:, col] = result[col].str.upper()
+            result.loc[:, col] = result[col].str.strip()
+        result.columns = self.result_columns
+        return PreProcessedData(result,events)
+
+
+    def _unifilter_report_preprocessing(self, data: pd.Series, **kwargs) -> PreProcessedData:
+        """report is same exact format as standard"""
+        return self._standard_report_preprocessing(data,**kwargs)
+
+
+    def preprocess(self, **kwargs) -> PreProcessedData:
+        method_by_name = {
+            "standard": self._standard_report_preprocessing,
+            "unifilter": self._unifilter_report_preprocessing,
+        }
+        preprocess_method = method_by_name.get(self.report_name, None)
+        if preprocess_method:
+            return preprocess_method(self.file.to_df(pdf="text"), **kwargs)

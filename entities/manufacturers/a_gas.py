74d7ef7c8aac5c177c9c2649bfacce1cac12a8eb
carboni-joseph <joe@carbonitech.com>
2023-02-02 13:59:43 -0500

    hndl no state in the address field, col len chngd

--- entities/manufacturers/a_gas.py
+++ entities/manufacturers/a_gas.py
@@ -16,26 +16,43 @@ class PreProcessor(AbstractPreProcessor):
         city_name_col: str = "City"
         state_name_col: str = "State"
         city_state_col: int = 1
-        inv_col: int = 6
-        comm_col: int = 9
+        inv_col: int = 7
+        comm_col: int = 10
+        no_state: bool = False
 
         data = data.T.reset_index().T.reset_index()  # move headers to first row and keep row index intact
         events.append(("Formatting","moved headers to first row. Headers now integers",self.submission_id))
-        data[[city_name_col, state_name_col]] = data.pop(city_state_col).str.split(", ", expand=True)
+        try:
+            city_state_data =  data.pop(city_state_col)
+            data[[city_name_col, state_name_col]] = city_state_data.str.split(", ", expand=True)
+        except ValueError:
+            # assuming this error means the state was not included in the data, only city.
+            data[city_name_col] = city_state_data
+            no_state = True
         events.append(("Formatting",f"split city-state, column {city_state_col}, into {city_name_col} and {state_name_col} columns",self.submission_id))
         data = data.dropna(subset=city_name_col)
         events.append(("Formatting",f"removed all rows with no values in the {city_name_col} column",self.submission_id))
-        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
+        if no_state:
+            result = data.loc[:,[customer_name_col, city_name_col, inv_col, comm_col]]
+        else:    
+            result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
 
-        result[inv_col] = result[inv_col].replace(r'[^.0-9]','',regex=True).astype(float) # convert string currency figure to float
-        result[comm_col] = result[comm_col].replace(r'[^.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result[inv_col] = result[inv_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result[comm_col] = result[comm_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
         result.loc[:,inv_col] = result[inv_col]*100
         result.loc[:,comm_col] = result[comm_col]*100
         for col in [customer_name_col,city_name_col,state_name_col]:
+            if col == state_name_col and no_state:
+                continue
             result.loc[:, col] = result[col].str.upper()
             result.loc[:, col] = result[col].str.strip()
-        result.columns = self.result_columns
-        result.index
+        
+        col_names = self.result_columns
+        if no_state:
+            col_names.pop(2) # remove "state"
+
+        result.columns = col_names
+        result = result.dropna(axis=1, how="all") # drop blank state column if state not in the report
         return PreProcessedData(result,events)
 
 

cb4c9bfe9f1a409624dd7c26904f8a726c17d0f9
carboni-joseph <joe@carbonitech.com>
2023-02-15 15:58:37 -0500

    new branch force upd w loc_id and reprocs err# 3

--- app/listeners/api_adapter_listener.py
+++ app/listeners/api_adapter_listener.py
@@ -1,6 +1,7 @@
 import db.models as model
 from app import event, report_processor
 from services.api_adapter import ApiAdapter, User
+from sqlalchemy.orm import Session
 from entities import error
 
 api = ApiAdapter()
@@ -8,6 +9,30 @@ api = ApiAdapter()
 """
 BUG: session is referred to as 'session' in one method and 'db' in the other
 """
+
+def update_new_customer_branch_with_loc_id(session: Session, branch_id: int) -> bool:
+    stmt = """
+        UPDATE customer_branches
+        SET location_id = subq.id
+        FROM (
+            SELECT loc.id
+            FROM customer_branches as cb
+            JOIN cities as ci
+            ON ci.id = cb.city_id
+            JOIN states as st
+            ON st.id = cb.state_id
+            JOIN locations as loc on loc.city = ci.name and loc.state = st.name
+            WHERE cb.id = :branch_id
+            LIMIT 1) as subq
+        WHERE customer_branches.id = :branch_id;
+    """
+    try:
+        session.execute(stmt, params={"branch_id": branch_id})
+        session.commit()
+    except:
+        return False
+    else:
+        return True
 
 def entity_default_name_mapping(table: model.Base, *args, **kwargs):
     """
@@ -44,6 +69,9 @@ def trigger_reprocessing_of_errors(table: model.Base, *args, **kwargs):
         error_type = error.ErrorType(3)
     elif table == model.CustomerBranch:
         error_type = error.ErrorType(4)
+        assert update_new_customer_branch_with_loc_id(session, kwargs.get("id_"))
+        # doing this to account for state lookup strategy using existing branches when only city, not state, is given
+        trigger_reprocessing_of_errors(model.MapStateName, *args, **kwargs)
     
     if error_type:
         errors = api.get_errors(session, user)

708bb2d68bb259818356865373fe28bdb2ed1334
carboni-joseph <joe@carbonitech.com>
2023-02-15 15:58:53 -0500

    e-p for branch creation

--- app/resources/branches.py
+++ app/resources/branches.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from services.api_adapter import ApiAdapter, get_db, User, get_user
-from app.jsonapi import convert_to_jsonapi, Query, JSONAPIRoute, BranchModificationRequest
+from app.jsonapi import convert_to_jsonapi, Query, JSONAPIRoute, BranchModificationRequest, RequestModels
 
 api = ApiAdapter()
 router = APIRouter(prefix="/branches", route_class=JSONAPIRoute)
@@ -20,9 +20,9 @@ async def branch_by_id(branch_id: int, query: Query=Depends(), db: Session=Depen
 async def modify_branch(branch_id: int, branch: BranchModificationRequest, db: Session=Depends(get_db), user: User=Depends(get_user)):
     return api.modify_branch(db, branch_id, branch.dict(), user)
 
-@router.post("", tags=['branches'], status_code=204)
-async def new_branch_by_customer_id():
-    return
+@router.post("", tags=['branches'])
+async def new_branch(jsonapi_obj: RequestModels.new_branch, db: Session=Depends(get_db), user: User=Depends(get_user)):
+    return api.create_branch(db=db, json_data=jsonapi_obj.dict(), user=user)
 
 @router.delete("/{branch_id}", tags=['branches'])
 async def delete_branch_by_id(branch_id: int, db: Session=Depends(get_db), user: User=Depends(get_user)):

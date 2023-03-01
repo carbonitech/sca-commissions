cc523c95d897d8332d4144d4f2dda72532f01660
carboni-joseph <joe@carbonitech.com>
2023-03-01 13:28:53 -0500

    convert paren'd vals to negative

--- entities/manufacturers/a_gas.py
+++ entities/manufacturers/a_gas.py
@@ -20,24 +20,24 @@ class PreProcessor(AbstractPreProcessor):
         data = data.dropna(subset=data.columns[city_name_col])
         result = data.iloc[:,[customer_name_col, city_name_col, inv_col, comm_col]]
 
-        customer_name_col: int = 0
-        city_name_col: int = 1
-        inv_col: int = 2
-        comm_col: int = 3
-
-        result.iloc[:,inv_col] = result.iloc[:,inv_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
-        result.iloc[:,comm_col] = result.iloc[:,comm_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
-        result.iloc[:,inv_col] = result.iloc[:,inv_col]*100
-        result.iloc[:,comm_col] = result.iloc[:,comm_col]*100
-        for col in [customer_name_col,city_name_col]:
-            result.iloc[:, col] = result.iloc[:,col].str.upper()
-            result.iloc[:, col] = result.iloc[:,col].str.strip()
-        
         col_names = self.result_columns.copy()
         col_names.pop(2) # remove "state"
-
         result.columns = col_names
+
+        customer_name_col, city_name_col, inv_col, comm_col = col_names
+
+        result.loc[:, inv_col] = result[inv_col].replace(r'^\(','-', regex=True)
+        result.loc[:, inv_col] = result[inv_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result.loc[:, comm_col] = result[comm_col].replace(r'^\(','-', regex=True)
+        result.loc[:, comm_col] = result[comm_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result.loc[:, inv_col] = result.loc[:, inv_col]*100
+        result.loc[:, comm_col] = result.loc[:, comm_col]*100
+        for col in [customer_name_col,city_name_col]:
+            result.loc[:, col] = result[col].str.upper()
+            result.loc[:, col] = result[col].str.strip()
+        
         result = result.dropna(axis=1, how="all") # drop blank state column if state not in the report
+        print(result)
         return PreProcessedData(result,events)
 
 

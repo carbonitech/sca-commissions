74b75a03ead20e9e4ab4931c86dcf834e17dc634
carboni-joseph <joe@carbonitech.com>
2023-03-14 17:53:51 -0400

    accepts skip rg for skipping rows or pages

--- entities/commission_file.py
+++ entities/commission_file.py
@@ -8,7 +8,7 @@ from io import BytesIO
 class CommissionFile:
     file_data: bytes|BytesIO
 
-    def to_df(self, combine_sheets=False, split_sheets=False, pdf: str=None) -> pd.DataFrame:
+    def to_df(self, combine_sheets=False, split_sheets=False, pdf: str=None, skip: int=0) -> pd.DataFrame:
         """
         read only visible sheets in the excel file
         if combine_sheets is True, attempt to UNION all visible sheets
@@ -28,28 +28,26 @@ class CommissionFile:
                 text_list_compact = [line.strip() for line in text_list if line.strip()]
                 return pd.Series(text_list_compact)
             elif strategy.lower() == "table":
-                return tabula.read_pdf(BytesIO(self.file_data), pages="all")[0]
+                return tabula.read_pdf(BytesIO(self.file_data), pages="all")[skip]
 
         try:
             with pd.ExcelFile(self.file_data, engine="openpyxl") as excel_file:
                 excel_file: pd.ExcelFile
                 visible_sheets = [sheet.title for sheet in excel_file.book.worksheets if sheet.sheet_state == "visible"]
                 if combine_sheets:
-                    data = [excel_file.parse(sheet) for sheet in visible_sheets]
+                    data = [excel_file.parse(sheet, skiprows=skip) for sheet in visible_sheets]
                     return pd.concat(data, ignore_index=True)
                 if split_sheets:
-                    return {sheet: excel_file.parse(sheet) for sheet in visible_sheets}
-                    
-
+                    return {sheet: excel_file.parse(sheet, skiprows=skip) for sheet in visible_sheets}
                 
-                return pd.read_excel(self.file_data, sheet_name=visible_sheets[0])
+                return pd.read_excel(self.file_data, sheet_name=visible_sheets[0], skiprows=skip)
         except:
             # TODO make sure this fallback using xlrd also ignores hidden sheets
             with pd.ExcelFile(self.file_data, engine="xlrd") as excel_file:
                 excel_file: pd.ExcelFile
                 if combine_sheets:
-                    data = [excel_file.parse(sheet) for sheet in excel_file.sheet_names]
+                    data = [excel_file.parse(sheet, skiprows=skip) for sheet in excel_file.sheet_names]
                     return pd.concat(data, ignore_index=True)
                 if split_sheets:
-                    return {sheet: excel_file.parse(sheet) for sheet in excel_file.sheet_names}
-                return pd.read_excel(self.file_data)
+                    return {sheet: excel_file.parse(sheet, skiprows=skip) for sheet in excel_file.sheet_names}
+                return pd.read_excel(self.file_data, skiprows=skip)

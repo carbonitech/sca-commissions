1254a5b971fa80dfe29e0b6e1f1c6e4359a78bf7
carboni-joseph <joe@carbonitech.com>
2023-03-14 17:55:02 -0400

    converted & now handle raw instead of templated

--- entities/manufacturers/allied.py
+++ entities/manufacturers/allied.py
@@ -7,46 +7,46 @@ Remarks:
 import pandas as pd
 from entities.commission_data import PreProcessedData
 from entities.preprocessor import AbstractPreProcessor
+from numpy import nan
 
 class PreProcessor(AbstractPreProcessor):
 
     def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the Allied standard report"""
 
-        events = []
-        customer_name_col: str = "Customer"
-        city_name_col: str = "City"
-        state_name_col: str = "State"
-        inv_col: str = "Sales"
-        # required_cols: set = {customer_name_col, city_name_col, state_name_col, inv_col}
+        customer_name_col: str = "customer"
+        inv_col: str = "inv_amt"
         comm_rate: float = kwargs.get("standard_commission_rate", 0)
+        # replace dashes so empty col removal works, drop empty cols, then leave out grand totals for rows and cols
+        data = data.replace('-',nan).dropna(how="all",axis=1).iloc[:-1,:-1]
+        data.columns = [customer_name_col, inv_col]
 
-        # if not required_cols.issubset(set(data.columns.tolist())):
-        #     first_header_row = data.index[data.iloc[:,3] == invoice_number_col].item()
-        #     data.columns = data.loc[first_header_row]
-        #     data = data.drop(first_header_row).reset_index(drop=True)
+        data.loc[:,inv_col] = data[inv_col]*100
+        data.loc[:,"commission"] = data[inv_col]*comm_rate
+        for col in [customer_name_col]:
+            data.loc[:, col] = data[col].str.upper()
+            data.loc[:, col] = data[col].str.strip()
+        
+        # get duplicated names
+        dup_names = data[data.duplicated(subset="customer")]["customer"].astype(str)
+        # create a sequence column by reseting the index twice and reinstating the original index
+        dup_names.reset_index().reset_index().set_index("index")
+        # add the suffix as a hyphen and the sequence number
+        dup_names["customer"] += "-" + dup_names["level_0"].astype(str)
+        # selectively replace values with suffixed values by index-label
+        data.loc[dup_names.index,"customer"] = dup_names["customer"]
 
-        data = data.rename(columns=lambda col: col.strip())
-        data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
-        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col]]
-        result.loc[:,inv_col] = result[inv_col]*100
-        result.loc[:,"commission"] = result[inv_col]*comm_rate
-        events.append(("Formatting",f"added commissions column by calculating {comm_rate*100:,.2f}% of the inv_amt",
-            self.submission_id))
-        for col in [customer_name_col,city_name_col,state_name_col]:
-            result.loc[:, col] = result[col].str.upper()
-            result.loc[:, col] = result[col].str.strip()
-        result.columns = self.result_columns
-        return PreProcessedData(result,events)
+        data["id_string"] = data[customer_name_col]
+        data.columns = ["customer","inv_amt","comm_amt","id_string"]
+        return PreProcessedData(data)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "standard": self._standard_report_preprocessing,
+            "standard": (self._standard_report_preprocessing, 1),
         }
-        preprocess_method = method_by_name.get(self.report_name, None)
+        preprocess_method, skip_param = method_by_name.get(self.report_name, None)
         if preprocess_method:
-            return preprocess_method(self.file.to_df(), **kwargs)
+            return preprocess_method(self.file.to_df(skip=skip_param, pdf="table"), **kwargs)
         else:
             return

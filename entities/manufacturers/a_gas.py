880d4dfd10932ff85b16cc29318d7598a2e9c652
carboni-joseph <joe@carbonitech.com>
2023-03-15 15:31:35 -0400

    formatting

--- entities/manufacturers/a_gas.py
+++ entities/manufacturers/a_gas.py
@@ -23,10 +23,14 @@ class PreProcessor(AbstractPreProcessor):
         customer_name_col, city_name_col, inv_col, comm_col = result.columns.tolist()
 
 
+        # convert string currency figure to float
         result.loc[:, inv_col] = result[inv_col].replace(r'^\(','-', regex=True)
-        result.loc[:, inv_col] = result[inv_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result.loc[:, inv_col] = result[inv_col].replace(r'[^-.0-9]','',regex=True).astype(float)
+
+        # convert string currency figure to float
         result.loc[:, comm_col] = result[comm_col].replace(r'^\(','-', regex=True)
-        result.loc[:, comm_col] = result[comm_col].replace(r'[^-.0-9]','',regex=True).astype(float) # convert string currency figure to float
+        result.loc[:, comm_col] = result[comm_col].replace(r'[^-.0-9]','',regex=True).astype(float)
+
         result.loc[:, inv_col] = result.loc[:, inv_col]*100
         result.loc[:, comm_col] = result.loc[:, comm_col]*100
         for col in [customer_name_col,city_name_col]:

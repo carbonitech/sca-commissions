acf427390f40d198411af23b42a2880d04a4f7bf
carboni-joseph <joe@carbonitech.com>
2023-03-15 15:32:12 -0400

    converted

--- entities/manufacturers/ambro_controls.py
+++ entities/manufacturers/ambro_controls.py
@@ -11,20 +11,16 @@ class PreProcessor(AbstractPreProcessor):
     def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the Ambro Controls standard report"""
 
-        events = []
-        customer_name_col: str = "customer name"
-        city_name_col: str = "ship to city"
+        customer_name_col: str = "customername"
+        city_name_col: str = "shiptocity"
         state_name_col: str = "state"
-        state_name_col_alt: str = "ship to state"
+        state_name_col_alt: str = "shiptostate"
         inv_col: str = "amount"
-        comm_col: str = "commission payable"
+        comm_col: str = "commissionpayable"
 
+        active_state_col = state_name_col # switch will flip to the alternate if there's an error
 
-        active_state_col = state_name_col
-
-        data = data.rename(columns=lambda col: col.strip().lower())
         data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
         try: 
             result = data.loc[:,[customer_name_col, city_name_col, active_state_col, inv_col, comm_col]]
         except KeyError:
@@ -35,8 +31,9 @@ class PreProcessor(AbstractPreProcessor):
         for col in [customer_name_col,city_name_col,active_state_col]:
             result.loc[:, col] = result[col].str.upper()
             result.loc[:, col] = result[col].str.strip()
-        result.columns = self.result_columns
-        return PreProcessedData(result,events)
+        result["id_string"] = result[[customer_name_col, city_name_col, active_state_col]].apply("_".join, axis=1)
+        result.columns = ["customer","city", "state","inv_col", "comm_col", "id_string"]
+        return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:

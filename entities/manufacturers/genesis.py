178ab279b0070ce0d89be5a9e037c9d7ef7a152c
carboni-joseph <joe@carbonitech.com>
2023-03-28 14:00:12 -0400

    convert and implement rebate report

--- entities/manufacturers/genesis.py
+++ entities/manufacturers/genesis.py
@@ -11,20 +11,23 @@ class PreProcessor(AbstractPreProcessor):
     def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the 'Sales Detail' genesis file"""
 
-        events = []
-        customer_name_col: str = "Cust Name"
-        city_name_col: str = "City"
-        state_name_col: str = "State"
-        inv_col: str = "Amount"
-        comm_col: str = "Comm Due"
+        customer_name_col: str = "custname"
+        city_name_col: str = "city"
+        state_name_col: str = "state"
+        inv_col: str = "amount"
+        comm_col: str = "commdue"
 
         data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
         result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
         result.loc[:,inv_col] = result[inv_col]*100
         result.loc[:,comm_col] = result[comm_col]*100
-        result.columns = self.result_columns # local result.cols are same length and position as self.result_columns
-        return PreProcessedData(result,events)
+        col_names = ["customer", "city", "state", "inv_amt", "comm_amt"]
+        result.columns = col_names
+        for col in col_names[:3]:
+            result.loc[:, col] = result[col].str.upper()
+            result.loc[:, col] = result[col].str.strip()
+        result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
+        return PreProcessedData(result)
 
 
     def _baker_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
@@ -35,120 +38,110 @@ class PreProcessor(AbstractPreProcessor):
             - split is applied to both sales and commission
         """
 
-        events = []
-        customer_name_col: str = "Customer Name"
-        city_name_col: str = "City"
-        state_name_col: str = "St."
-        inv_col: str = "Amount"
-        comm_col: str = "Comm.  Due"
+        customer_name_col: str = "customername"
+        city_name_col: str = "city"
+        state_name_col: str = "st."
+        inv_col: str = "amount"
+        comm_col: str = "comm.due"
 
         split: float = kwargs.get("split", 1.0)
-        default_branch: dict[str,str] = kwargs.get("default_branch")
+        comm_ref_col: str   # will set which column to use for the commission calculation
+
+        ## the following is a switch to flip in case we're dealing with a Lennox report
+        if kwargs.get("lennox"):
+            alt_split = 0.01
+            comm_ref_col = inv_col
+        else:
+            alt_split = 100*split
+            comm_ref_col = comm_col
 
         data = data.dropna(axis=1, how='all')
-        events.append(("Formatting","removed columns with no values",self.submission_id))
         data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
         result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
         result.loc[:,inv_col] = result[inv_col]*100*split
-        result.loc[:,comm_col] = result[comm_col]*100*split
-        events.append(("Formatting",f"replaced {inv_col} column with {split*100:,.2f}% of the values",self.submission_id))
-        events.append(("Formatting",f"replaced {comm_col} column with {split*100:,.2f}% of the values",self.submission_id))
+        result.loc[:,comm_col] = result[comm_ref_col]*alt_split
         
         # a default is used to credit all sales data to agency. Locations in the data are not territory-specific,
         # although they're used to calculate the values
         total_inv = result[inv_col].sum()
         total_comm = result[comm_col].sum()
-        result = pd.DataFrame(
-                [
-                    list(default_branch.values())
-                    +[total_inv, total_comm]
-                ],
-            columns=self.result_columns)
-
-        return PreProcessedData(result,events)
+        col_names = ["inv_amt", "comm_amt"]
+        result = pd.DataFrame([[total_inv, total_comm]], columns=col_names)
+        result["id_string"] = ""
+        return PreProcessedData(result)
 
 
     def _lennox_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """
-        processes the 'Lennox' genesis file
-        Remarks:
-        - Lennox report does not have commission amounts in the actual table, only sales.
-        - Sales will be scaled down by the split proportion to determine sales
-        - Commission amount will be 1% of sales post-split. Column starts as all zeros
-        - Entire amount is credited to one customer_branch
+            Processing is nearly identical to Baker's report, except minor tweaks to the use
+            of commission split values. The tables have the same structure and headings after extraction
+
+            Setting `lennox=True` will induce the following method to use commission column calculations
+            appropriate for the Lennox report
         """
-
-        events = []
-        customer_name_col: str = "Customer Name"
-        city_name_col: str = "City"
-        state_name_col: str = "St."
-        inv_col: str = "Amount"
-        comm_col: str = "Comm.  Due"
-
-        split: float = kwargs.get("split", 1.0)
-        default_branch: dict[str,str] = kwargs.get("default_branch")
-
-        data = data.dropna(axis=1, how='all')
-        events.append(("Formatting","removed columns with no values",self.submission_id))
-        data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
-        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
-        result.loc[:,inv_col] = result[inv_col]*100*split
-        result.loc[:,comm_col] = result[inv_col]*0.01 # all reps in this report get 1%, it's the split that varies
-        events.append(("Formatting",f"replaced {inv_col} column with {split*100:,.2f}% of the values",self.submission_id))
-        events.append(("Formatting",f"replaced {comm_col} column with {0.01*100:,.2f}% of sales",self.submission_id))
-
-        # a default is used to credit all sales data to agency. Locations in the data are not territory-specific,
-        # although they're used to calculate the values
-        total_inv = result[inv_col].sum()
-        total_comm = result[comm_col].sum()
-        result = pd.DataFrame(
-                [
-                    list(default_branch.values())
-                    +[total_inv, total_comm]
-                ],
-            columns=self.result_columns)
-
-        return PreProcessedData(result,events)
-
+        return self._baker_report_preprocessing(data, lennox=True, **kwargs)
 
     def _winsupply_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         
-        events = []
-        customer_name_col: str = "bill to address 1"
-        city_name_col: str = "bill to address 4"
-        state_name_col: str = "bill to state"
-        inv_col: str = "mtd sales $"
+        customer_name_col: str = "billtoaddress1"
+        city_name_col: str = "billtoaddress4"
+        state_name_col: str = "billtostate"
+        inv_col: str = "mtdsales$"
         comm_col: str = "comm_amt" 
-        comm_col_index: int = -1 # commission is last column but isn't named properly
+        comm_col_index: int = -1 # commission column has an unreliable name
         
-
-        data = data.rename(columns=lambda col: col.strip().lower())
         data = data.dropna(axis=1, how='all')
         data = data.dropna(subset=data.columns.to_list()[0])
         data.loc[:,comm_col] = data.iloc[:,comm_col_index]
         result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
         result.loc[:,inv_col] = result[inv_col]*100
         result.loc[:,comm_col] = result[comm_col]*100
-        result.columns = self.result_columns
-        return PreProcessedData(result,events)
+        col_names = ["customer", "city", "state", "inv_amt", "comm_amt"]
+        result.columns = col_names
+        for col in col_names[:3]:
+            result.loc[:, col] = result[col].str.upper()
+            result.loc[:, col] = result[col].str.strip()
+        result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
+        return PreProcessedData(result)
 
     def _rebate_detail_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
-        ...
+
+        customer_name_col: str = 'customername'
+        customer_name_index: int = data.columns.to_list().index(customer_name_col)
+        inv_col_index: int = -2
+        comm_col_index: int = -1
+
+        result = data.iloc[:,[customer_name_index, inv_col_index, comm_col_index]]
+
+        data = data.dropna(axis=1, how='all')
+        data = data.dropna(subset=data.columns.to_list()[0])
+
+        # negating (cent) values to reflect rebate as a refund
+        result.iloc[:,inv_col_index] *= -100
+        result.iloc[:,comm_col_index] *= -100
+
+        result.loc[:, customer_name_col] = result[customer_name_col].str.upper()
+        result.loc[:, customer_name_col] = result[customer_name_col].str.strip()
+
+        col_names = ["customer", "inv_amt", "comm_amt"]
+        result.columns = col_names
+        result["id_string"] = result[col_names[0]]
+        
+        return PreProcessedData(result)
+
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "sales_detail": self._standard_report_preprocessing,
-            "baker_pos": self._baker_report_preprocessing,
-            "lennox_pos": self._lennox_report_preprocessing,
-            "winsupply_pos": self._winsupply_report_preprocessing,
-            "rebate_detail": self._rebate_detail_report_preprocessing
+            "sales_detail": (self._standard_report_preprocessing,0),
+            "baker_pos": (self._baker_report_preprocessing,33),
+            "lennox_pos": (self._lennox_report_preprocessing,45),
+            "winsupply_pos": (self._winsupply_report_preprocessing,0),
+            "rebate_detail": (self._rebate_detail_report_preprocessing,0),
         }
-        preprocess_method = method_by_name.get(self.report_name, None)
+        preprocess_method, skip_param = method_by_name.get(self.report_name, None)
         if preprocess_method:
             if self.report_name == "winsupply_pos":
                 return preprocess_method(self.file.to_df(combine_sheets=True), **kwargs)
-            return preprocess_method(self.file.to_df(), **kwargs)
+            return preprocess_method(self.file.to_df(skip=skip_param), **kwargs)
         else:
             return

94805389b3c2c9312a3b68164c68254a715120e9
carboni-joseph <joe@carbonitech.com>
2023-03-30 06:14:10 -0400

    removed endpoints

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -2,13 +2,8 @@ from app.resources.commissions import router as commissions
 from app.resources.branches import router as branches
 from app.resources.customers import router as customers
 from app.resources.manufacturers import router as manufacturers
-from app.resources.mappings import router_customers as customer_names 
-from app.resources.mappings import router_cities as city_names
-from app.resources.mappings import router_states as state_names
 from app.resources.reps import router as reps
 from app.resources.submissions import router as submissions
-from app.resources.cities import router as cities
-from app.resources.states import router as states
 from app.resources.relationships import router as relationships
 from app.resources.download import router as downloads
 from app.resources.reports import router as reports

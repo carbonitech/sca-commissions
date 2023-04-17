695b8d4e476a3587df5c7640b1c34fd8c5985fdc
carboni-joseph <joe@carbonitech.com>
2023-04-17 12:33:01 -0400

    retain line items not matched in sales rpt

--- entities/manufacturers/c&d_valve.py
+++ entities/manufacturers/c&d_valve.py
@@ -20,9 +20,11 @@ class PreProcessor(AbstractPreProcessor):
         """
         invoice_number_col: str = "invoicenumber"
         comm_col: int = -1
+        customer_col: int = 2
+        inv_col_comm: int = -3
 
         sales_report: pd.DataFrame = pd.read_excel(kwargs.get("additional_file_1"))\
-                    .rename(columns=lambda col: col.lower().replace(" "))
+                    .rename(columns=lambda col: col.lower().replace(" ",""))
         customer_name_col: int = 4
         city_name_col: str = "city"
         state_name_col: str = "state"
@@ -35,15 +37,18 @@ class PreProcessor(AbstractPreProcessor):
             first_header_row = data.index[data.iloc[:,3] == invoice_number_col].item()
             data.columns = data.loc[first_header_row]
             data = data.drop(first_header_row).reset_index(drop=True)
-        data = pd.concat([data.loc[:,invoice_number_col], data.iloc[:,comm_col]], axis=1).dropna()
-        data.columns = ["invoice_summary","total_comm"]
+        data = pd.concat([data.loc[:,invoice_number_col], data.iloc[:,[customer_col,inv_col_comm,comm_col]]], axis=1).dropna()
+        data.columns = ["invoice_summary","customer","inv_amt","comm_amt"]
         data["invoice_summary"] = data["invoice_summary"].astype(int)
-        data["total_comm"] = data["total_comm"].astype(float)
+        data["inv_amt"] = data["inv_amt"].astype(float)*100
+        data["total_comm"] = data["total_comm"].astype(float)*100
 
         # in the sales report, inoice numbers are in their own column and share the same index as customer info and sales figure
         invoice_numbers = sales_report.iloc[:,invoice_num_col].dropna()
         if "Invoice Number" in invoice_numbers.values:
             invoice_numbers = invoice_numbers.iloc[1:]
+            
+            
         value_indecies = invoice_numbers.index.to_list()
         sales_report = sales_report.loc[value_indecies]
         sales_report[sales_report.columns[invoice_num_col]] = sales_report.iloc[:,invoice_num_col].astype(int)
@@ -56,21 +61,20 @@ class PreProcessor(AbstractPreProcessor):
         sales_report = sales_report.iloc[:,[0,1,3,4,2]]
         sales_report.columns = ["invoice", "customer", "city", "state", "inv_amt"]
         sales_report.loc[:, "inv_amt"] *= 100
-        result = sales_report.groupby(sales_report.columns[:4]).sum().reset_index()
+        result = sales_report.groupby(list(sales_report.columns[:4])).sum().reset_index()
         
         # merge the sales report data with the commission report data by invoice number
-        merged = result.merge(data, how="left", left_on="invoice", right_on="invoice_summary")
-        result.loc[:,"comm_amt"] = merged["total_comm"]*100
+        merged = data.merge(result, how="left", left_on="invoice_summary", right_on="invoice", suffixes=(None,"_1"))
+        id_cols = ["customer", "city", "state"]
+        merged["id_string"] = merged[id_cols].fillna("").apply("_".join, axis=1)
         # drop invoice number column
-        result = result.drop(columns="invoice")
-
+        result_cols = ["id_string", "inv_amt", "comm_amt"]
+        result = merged.loc[:,result_cols]
         # standardize
-        result.columns = ["customer", "city", "state", "inv_amt", "comm_amt"]
         result = result.apply(self.upper_all_str)
-
         # without invoice column now, group by branch
-        result = result.groupby(result.columns[:3]).sum().reset_index()
-        result["id_string"] = result[result.columns.tolist()[:3]].apply("_".join, axis=1)
+        result = result.groupby("id_string").sum().reset_index()
+        
         return PreProcessedData(result)
 
 

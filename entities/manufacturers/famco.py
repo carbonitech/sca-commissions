c2ddd74f5abf6f664626c7fadcac28c49327d4d5
carboni-joseph <joe@carbonitech.com>
2023-04-18 13:48:01 -0400

    bugfixes - was returning empty frame

--- entities/manufacturers/famco.py
+++ entities/manufacturers/famco.py
@@ -11,24 +11,21 @@ class PreProcessor(AbstractPreProcessor):
     def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the Famco standard report"""
 
-        customer_name_col: str = "shiptoname"
-        city_name_col: str = "shiptocity"
-        inv_col_index: int = 13
-        inv_col: str = "sales"
-        comm_col: str = "extcom"
+        # headers are lost once the df is condensed
+        customer_name_col: int = 2
+        city_name_col: int = 3
+        inv_col: int = 11
+        comm_col: int = -1
 
         data = data.dropna(how="all",axis=1)
         # condense the table by removing all empty cells by column and then recombining them
         data = pd.concat([data[col].dropna() for col in data.columns.to_list()], axis=1, ignore_index=True)
-        # move first row up to the headers
-        data.columns = data.iloc[0].str.lower()
-        data = data.iloc[1:].reset_index(drop=True)
-        data = data.dropna(subset=data.columns.to_list()[0])
-        data = data.rename(columns={data.columns[inv_col_index]: inv_col})
-        result = data.loc[:,[customer_name_col, city_name_col, inv_col, comm_col]]
+        data = data.reset_index(drop=True)
+        data = data.dropna(subset=data.columns[0])
+        result = data.iloc[:,[customer_name_col, city_name_col, inv_col, comm_col]]
 
-        result.loc[:,inv_col] *= 100
-        result.loc[:,comm_col] *= 100
+        result.iloc[:,inv_col] *= 100
+        result.iloc[:,comm_col] *= 100
 
         result = result.apply(self.upper_all_str)
         

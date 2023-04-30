c640b35bd9ab70436019fa89703f7a095353e8e7
carboni-joseph <joe@carbonitech.com>
2023-04-30 16:16:55 -0400

    support tjernlund

---
+++ entities/manufacturers/tjernlund.py
@@ -0,0 +1,40 @@
+"""
+Manufacturer report preprocessing definition
+for Tjernlund
+"""
+import pandas as pd
+from entities.commission_data import PreProcessedData
+from entities.preprocessor import AbstractPreProcessor
+
+class PreProcessor(AbstractPreProcessor):
+
+    def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+        """processes the Tjernlund standard report"""
+
+        customer_col: str = "address1"
+        inv_col: str = "extension"
+        comm_col_i: int = -1  # depends on removing trailing columns
+        comm_col: str = "comm_amt"
+
+        data = self.check_headers_and_fix([customer_col, inv_col], data)
+
+        data.iloc[:,5:] = data.iloc[:,5:].shift(-1) # line up customer names with values
+        data = data.dropna(subset=data.columns[0])\
+                    .dropna(axis=1, how="all")\
+                    .rename(columns={data.columns[comm_col_i]: comm_col})
+        data.loc[:,comm_col] = data[comm_col].astype(float)
+        data["id_string"] = data[customer_col]
+        result = data.loc[:,["id_string", inv_col, comm_col]]
+        
+        return PreProcessedData(result)
+
+
+    def preprocess(self, **kwargs) -> PreProcessedData:
+        method_by_name = {
+            "standard": self._standard_report_preprocessing,
+        }
+        preprocess_method = method_by_name.get(self.report_name, None)
+        if preprocess_method:
+            return preprocess_method(self.file.to_df(), **kwargs)
+        else:
+            return

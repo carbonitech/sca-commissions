cc8c09d7ca369d997f9e12b682776de316a8332e
carboni-joseph <joe@carbonitech.com>
2023-05-01 18:55:26 -0400

    fix col names, data types returned, & cond result

--- entities/manufacturers/allied.py
+++ entities/manufacturers/allied.py
@@ -20,24 +20,24 @@ class PreProcessor(AbstractPreProcessor):
         
         # replace dashes so empty col removal works, drop empty cols, then leave out grand totals for rows and cols
         data = data.replace('-',nan).dropna(how="all",axis=1).iloc[:-1,:-1]
+        data = data.iloc[:,[0,-1]]  # get most recent month only
         data.columns = [customer_name_col, inv_col]
 
-        data.loc[:,inv_col] *= 100
-        data.loc[:,"commission"] = data[inv_col]*comm_rate
+        data[inv_col] = data[inv_col].replace('[^-.0-9]','', regex=True).astype(float)*100
+        data["comm_amt"] = data[inv_col]*comm_rate
         data = data.apply(self.upper_all_str)
-        
         # get duplicated names
-        dup_names = data[data.duplicated(subset="customer")]["customer"].astype(str)
+        dup_names = data.loc[data.duplicated(subset="customer"), ["customer"]].astype(str)
         # create a sequence column by reseting the index twice and reinstating the original index
-        dup_names.reset_index().reset_index().set_index("index")
+        dup_names = dup_names.reset_index().reset_index().set_index("index")
         # add the suffix as a hyphen and the sequence number
-        dup_names["customer"] += "-" + dup_names["level_0"].astype(str)
+        dup_names["customer"] += "-" + dup_names["level_0"].astype(str) # level_0 is the column name assigned to the column created by the second reset_index call
         # selectively replace values with suffixed values by index-label
         data.loc[dup_names.index,"customer"] = dup_names["customer"]
 
         data["id_string"] = data[customer_name_col]
-        data.columns = ["customer","inv_amt","comm_amt","id_string"]
-        return PreProcessedData(data)
+        result = data.iloc[:,-3:].dropna()
+        return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:

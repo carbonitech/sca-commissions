e3e3e843abba83890d94c113e7b6f7af6a2633b5
carboni-joseph <joe@carbonitech.com>
2023-05-06 19:57:40 -0400

    no need to backfill loc id, moved db calls undr if

--- app/listeners/api_adapter_listener.py
+++ app/listeners/api_adapter_listener.py
@@ -10,47 +10,22 @@ api = ApiAdapter()
 BUG: session is referred to as 'session' in one method and 'db' in the other
 """
 
-def update_new_customer_branch_with_loc_id(session: Session, branch_id: int) -> bool:
-    stmt = """
-        UPDATE customer_branches
-        SET location_id = subq.id
-        FROM (
-            SELECT loc.id
-            FROM customer_branches as cb
-            JOIN cities as ci
-            ON ci.id = cb.city_id
-            JOIN states as st
-            ON st.id = cb.state_id
-            JOIN locations as loc on loc.city = ci.name and loc.state = st.name
-            WHERE cb.id = :branch_id
-            LIMIT 1) as subq
-        WHERE customer_branches.id = :branch_id;
-    """
-    try:
-        session.execute(stmt, params={"branch_id": branch_id})
-        session.commit()
-    except:
-        return False
-    else:
-        return True
-
 def trigger_reprocessing_of_errors(table: model.Base, *args, **kwargs):
     error_type = None
-    session = kwargs.get("session")
-    if not session:
-        session = kwargs.get("db") # hot fix
-    user = kwargs.get("user")
-    if table == model.CustomerBranch:
-        error_type = error.ErrorType(4)
-        assert update_new_customer_branch_with_loc_id(session, kwargs.get("id_"))
-
-    if table == model.IDStringMatch:
+    if table == model.CustomerBranch or table == model.IDStringMatch:
         error_type = error.ErrorType(4)
     
     if error_type:
+        session = kwargs.get("session")
+        if not session:
+            session = kwargs.get("db") # hot fix
+        user = kwargs.get("user")
         errors = api.get_errors(session, user)
-        processor = report_processor.ReportProcessor(session=session,target_err=error_type, error_table=errors, user=user)
-        processor.process_and_commit()
+        report_processor.ErrorReintegrationStrategy(
+            session=session,
+            target_err=error_type,
+            error_table=errors,
+            user=user).process_and_commit()
     return
 
 def setup_api_event_handlers():

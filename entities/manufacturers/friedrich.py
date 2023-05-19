35b9f72ca4cca89177dd4742bbe15e2de973e2ea
carboni-joseph <joe@carbonitech.com>
2023-05-19 17:15:51 -0400

    fix johnstone pos processing

--- entities/manufacturers/friedrich.py
+++ entities/manufacturers/friedrich.py
@@ -37,36 +37,28 @@ class PreProcessor(AbstractPreProcessor):
     def _johnstone_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the Friedrich Johnstone tab"""
 
-        events = []
         default_customer_name: str = "JOHNSTONE SUPPLY"
-        store_number_col: str = "Store Number"
-        city_name_col: str = "CustName"
-        state_name_col: str = "State"
-        inv_col: str = "NetSales"
-        comm_col: str = "Commission"
+        store_number_col: str = "storenumber"
+        city_name_col: str = "custname"
+        state_name_col: str = "state"
+        inv_col: str = "netsales"
+        comm_col: str = "commission"
 
-        data = data.dropna(subset=data.columns.to_list()[0])
-        events.append(("Formatting","removed all rows with no values in the first column",self.submission_id))
-        if city_name_col not in data.columns.to_list():
-            data = data.rename(columns=data.iloc[0]).drop(data.index[0])
+        data = data.dropna(subset=data.columns[0])
         data = data.dropna(how="all",axis=1)
-        events.append(("Formatting","removed columns with no values",self.submission_id))
         data[store_number_col] = data[store_number_col].astype(str)
         data[store_number_col] = data[store_number_col].str.strip()
-        data.loc[:,"customer"] = default_customer_name
-        events.append(("Formatting",f"added a column with customer name {default_customer_name} in all rows",
-            self.submission_id))
+        data["customer"] = default_customer_name
         result = data.loc[:,
             [store_number_col, "customer", city_name_col, state_name_col, inv_col, comm_col]
         ]
-
         result.loc[:,inv_col] *= 100
         result.loc[:,comm_col] *= 100
-        
+        result['id_string'] = result[[store_number_col, "customer", city_name_col, state_name_col]].fillna('').apply('_'.join, axis=1)
+        result = result.loc[:,['id_string', inv_col, comm_col]]
+        result = result.rename(columns={inv_col: "inv_amt", comm_col: 'comm_amt'})
         result = result.apply(self.upper_all_str)
-
-        result.columns = ["store_number"] + self.result_columns
-        return PreProcessedData(result,events)
+        return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:

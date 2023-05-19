ef7c59d31dea8844065f796b6e860459e7214a06
carboni-joseph <joe@carbonitech.com>
2023-05-19 16:41:50 -0400

    handle `county` as header - need better method

--- entities/manufacturers/ambro_controls.py
+++ entities/manufacturers/ambro_controls.py
@@ -14,29 +14,40 @@ class PreProcessor(AbstractPreProcessor):
         customer_name_col: str = "customername"
         city_name_col: str = "shiptocity"
         state_name_col: str = "state"
-        state_name_col_alt: str = "shiptostate"
+        state_name_col_alt0: str = "shiptostate"
+        state_name_col_alt1: str = "county"
         inv_col: str = "amount"
         comm_col: str = "commissionpayable"
 
         active_state_col = state_name_col # switch will flip to the alternate if there's an error
-
+        data = self.check_headers_and_fix([customer_name_col, city_name_col,inv_col], data)
         data = data.dropna(subset=data.columns.to_list()[0])
         try: 
             result = data.loc[:,[customer_name_col, city_name_col, active_state_col, inv_col, comm_col]]
         except KeyError:
-            active_state_col = state_name_col_alt
-            result = data.loc[:,[customer_name_col, city_name_col, active_state_col, inv_col, comm_col]]
+            active_state_col = state_name_col_alt0
+            try:
+                result = data.loc[:,[customer_name_col, city_name_col, active_state_col, inv_col, comm_col]]
+            except KeyError:
+                active_state_col = state_name_col_alt1
+                result = data.loc[:,[customer_name_col, city_name_col, active_state_col, inv_col, comm_col]]
+                
         result.loc[:,inv_col] *= 100
         result.loc[:,comm_col] *= 100
         result = result.apply(self.upper_all_str)
         result["id_string"] = result[[customer_name_col, city_name_col, active_state_col]].apply("_".join, axis=1)
-        result.columns = ["customer","city", "state","inv_col", "comm_col", "id_string"]
+        result.columns = ["customer","city", "state", "inv_amt", "comm_amt", "id_string"]
         return PreProcessedData(result)
+
+    def _re_michel_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+        """process the RE Michel report"""
+        raise NotImplementedError
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
             "standard": self._standard_report_preprocessing,
+            "re_michel_pos": self._re_michel_report_preprocessing
         }
         preprocess_method = method_by_name.get(self.report_name, None)
         if preprocess_method:

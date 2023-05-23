b884c2af9fd02934c523c84eb36e87656e077bad
carboni-joseph <joe@carbonitech.com>
2023-05-22 22:06:22 -0400

    elim some whitespace

--- services/post.py
+++ services/post.py
@@ -26,16 +26,13 @@ def create_customer(db: Session, json_data: dict, user: User) -> JSONAPIResponse
     json_data["data"]["attributes"]["name"] = new_name.upper().strip()
     return __create_X(db, json_data, user, CUSTOMERS)
 
-
 @jsonapi_error_handling
 def create_branch(db: Session, json_data: dict, user: User) -> JSONAPIResponse:
     return __create_X(db, json_data, user, BRANCHES)
 
-
 @jsonapi_error_handling
 def create_mapping(db: Session, json_data: dict, user: User) -> JSONAPIResponse:
     return __create_X(db, json_data, user, ID_STRINGS)
-
 
 @jsonapi_error_handling
 def create_manufacturer(db: Session, json_data: dict, user: User) -> JSONAPIResponse:

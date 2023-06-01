f2b42e2fc270eb5870f6ec8569f7a4b58914acb8
carboni-joseph <joe@carbonitech.com>
2023-06-01 15:08:40 -0400

    fix issue causing lost data

--- entities/manufacturers/tjernlund.py
+++ entities/manufacturers/tjernlund.py
@@ -12,14 +12,18 @@ class PreProcessor(AbstractPreProcessor):
         """processes the Tjernlund standard report"""
 
         customer_col: str = "address1"
+        invoice_num: str = 'invoice_no'
         inv_col: str = "extension"
         comm_col_i: int = -1  # depends on removing trailing columns
         comm_col: str = "comm_amt"
 
         data = self.check_headers_and_fix([customer_col, inv_col], data)
         data.iloc[:,5:] = data.iloc[:,5:].shift(-1) # line up customer names with values
-        data = data.dropna(subset=customer_col)\
-                    .dropna(axis=1, how="all")
+        data = (
+            data.dropna(subset=invoice_num)
+                .dropna(axis=1, how="all")
+        )
+        data[customer_col] = data[customer_col].ffill()
         data = data.rename(columns={data.columns[comm_col_i]: comm_col})
         data[comm_col] = data[comm_col].astype(float)
         data["id_string"] = data[customer_col]
@@ -27,7 +31,6 @@ class PreProcessor(AbstractPreProcessor):
         result[inv_col] *= 100
         result[comm_col] *= 100
         result = result.rename(columns={inv_col: "inv_amt"})
-        
         return PreProcessedData(result)
 
 

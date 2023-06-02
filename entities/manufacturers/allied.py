91fefba79333b29b8098ec4ec9bf6afcf4921ac2
carboni-joseph <joe@carbonitech.com>
2023-06-01 21:09:39 -0400

    fix issue with trunc read. manually parsing now

--- entities/manufacturers/allied.py
+++ entities/manufacturers/allied.py
@@ -5,48 +5,71 @@ Remarks:
     - This scheme will assume a file structure that would reasonably come from a Allied, while requiring very little pre-processing work
 """
 import pandas as pd
+import numpy as np
 from entities.commission_data import PreProcessedData
 from entities.preprocessor import AbstractPreProcessor
 from numpy import nan
 
 class PreProcessor(AbstractPreProcessor):
 
-    def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _standard_report_preprocessing(self, data: pd.Series, **kwargs) -> PreProcessedData:
         """processes the Allied standard report"""
 
         customer_name_col: str = "customer"
         inv_col: str = "inv_amt"
         comm_rate: float = kwargs.get("standard_commission_rate", 0)
         
-        # replace dashes so empty col removal works, drop empty cols, then leave out grand totals for rows and cols
-        data = data.replace('-',nan).dropna(how="all",axis=1).iloc[:-1,:-1]
-        data = data.iloc[:,[0,-1]]  # get most recent month only
-        data.columns = [customer_name_col, inv_col]
+        # reduce to a slice, using the series value that contains the "Sales Data" header, but leave out the header and totals rows
+        data: pd.Series = data[data.index[data.str.match(r'^\s*Sales Data')].tolist()[0]:].iloc[2:-1]
+        df = (
+            pd.DataFrame(data.str.split(r'\s{2,}', regex=True).to_list())       # best col delimiter for now is 2+ spaces
+                .replace('-', 0)
+                .fillna(0)
+                .replace(r'\(([0-9]*)\)', '-\\1', regex=True)       # numbers surrounded by parens are negative
+                .replace(r'([0-9])\,([0-9])', '\\1\\2', regex=True) # remove comma notation within numbers, using this regex because customer and first col of values are still combined
+        )
+       # the first column of numbers is separated only by 1 space from the customer name
+       # so we'll use a letter-space-number combo in the first col to separate and replace the first column
+        df = pd.concat([df.pop(0).str.extract(r'([^0-9]*)\s([0-9]*)', expand=True), df], axis=1, ignore_index=True)
+        df[df.columns[1:]] = df.iloc[:,1:].astype(int)
+        # if the last column has a 0, pull the right-most value over to the totals column
+        # and remove it from it's prior position in the table
+        replace_values: pd.Series = (
+            df.iloc[:,1:]
+                .apply(lambda row: [row.index[row>0].max(), row.loc[row.index[row > 0].max()]] if row.iloc[-1] == 0 else 0, axis=1)
+                .replace(0, np.nan)
+                .dropna()
+        )
+        if not replace_values.empty:
+            for index, val in replace_values.items():
+                source_index = val[0] 
+                dollar_amount = val[1]
+                df.loc[index,df.columns[source_index]] = 0
+                df.loc[index,df.columns[-1]] = dollar_amount
+        # keep only lastest month
+        df = df.iloc[:,[0,-2]]
 
-        data[inv_col] = data[inv_col].replace(r'\(','-', regex=True)
-        data[inv_col] = data[inv_col].replace(r'[^-.0-9]','', regex=True).astype(float)*100
-        data["comm_amt"] = data[inv_col]*comm_rate
-        data = data.apply(self.upper_all_str)
-        # get duplicated names
-        dup_names = data.loc[data.duplicated(subset="customer"), ["customer"]].astype(str)
-        # create a sequence column by reseting the index twice and reinstating the original index
+        df.columns = [customer_name_col, inv_col]
+        df[inv_col] *= 100
+        df["comm_amt"] = df[inv_col]*comm_rate
+        df = df.apply(self.upper_all_str)
+        # tag duplicated names with suffixes
+        dup_names = df.loc[df.duplicated(subset="customer"), ["customer"]].astype(str)
         dup_names = dup_names.reset_index().reset_index().set_index("index")
-        # add the suffix as a hyphen and the sequence number
         dup_names["customer"] += "-" + dup_names["level_0"].astype(str) # level_0 is the column name assigned to the column created by the second reset_index call
-        # selectively replace values with suffixed values by index-label
-        data.loc[dup_names.index,"customer"] = dup_names["customer"]
+        df.loc[dup_names.index,"customer"] = dup_names["customer"]
 
-        data["id_string"] = data[customer_name_col]
-        result = data.iloc[:,-3:].dropna()
+        df["id_string"] = df[customer_name_col]
+        result = df.iloc[:,-3:].dropna()
         return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "standard": (self._standard_report_preprocessing, 1),
+            "standard": self._standard_report_preprocessing,
         }
-        preprocess_method, skip_param = method_by_name.get(self.report_name, None)
+        preprocess_method = method_by_name.get(self.report_name, None)
         if preprocess_method:
-            return preprocess_method(self.file.to_df(skip=skip_param, pdf="table", combine_sheets=True), **kwargs)
+            return preprocess_method(self.file.to_df(pdf="text", combine_sheets=True), **kwargs)
         else:
             return

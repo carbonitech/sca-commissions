61d10514bba70992e64edb8f7d364a14286ef77e
carboni-joseph <joe@carbonitech.com>
2023-07-10 13:44:20 -0400

    bugfixes - dynamically find headers

--- entities/manufacturers/glasfloss.py
+++ entities/manufacturers/glasfloss.py
@@ -19,8 +19,9 @@ class PreProcessor(AbstractPreProcessor):
         total_freight: float = kwargs.get("total_freight_amount", None)
         comm_rate = kwargs.get("standard_commission_rate",0)
 
+        data = self.check_headers_and_fix([customer_name_col, city_name_col, state_name_col, inv_col], data)
         data = data.dropna(subset=data.columns.to_list()[1])
-        data.loc[:,inv_col] = data[inv_col]*100
+        data.loc[:,inv_col] *= 100
         if total_freight:
             total_sales = data[inv_col].sum()/100 # converted to dollars
             discount_rate = total_freight/total_sales
@@ -30,20 +31,21 @@ class PreProcessor(AbstractPreProcessor):
             data.loc[:,comm_col] = 0
 
         result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
-        col_names = ["customer", "city", "state", "inv_col", comm_col]
+        col_names = ["customer", "city", "state", "inv_amt", comm_col]
+        result.columns = col_names
         result = result.apply(self.upper_all_str)
         result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
-        result.columns = col_names
+        result = result[["id_string", "inv_amt", comm_col]]
 
         return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "standard": (self._standard_report_preprocessing, 2),
+            "standard": self._standard_report_preprocessing
         }
-        preprocess_method, skip_param = method_by_name.get(self.report_name, None)
+        preprocess_method = method_by_name.get(self.report_name, None)
         if preprocess_method:
-            return preprocess_method(self.file.to_df(skip=skip_param, treat_headers=True), **kwargs)
+            return preprocess_method(self.file.to_df(treat_headers=True), **kwargs)
         else:
             return

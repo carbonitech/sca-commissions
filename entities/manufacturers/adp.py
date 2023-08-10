a33621fe50d7d72419dd5ceec2b4730ad13f36f4
carboni-joseph <joe@carbonitech.com>
2023-08-10 11:06:35 -0400

    refactored

--- entities/manufacturers/adp.py
+++ entities/manufacturers/adp.py
@@ -20,22 +20,20 @@ class PreProcessor(AbstractPreProcessor):
 
     def _standard_report_preprocessing(self,data: pd.DataFrame, **kwargs) -> PreProcessedData:
         """processes the 'Detail' tab of the ADP commission report"""
-        customer_name_col: str = "customer.1"
-        city_name_col: str = "shiptocity"
-        state_name_col: str = "shptostate"
-        inv_col: str = "netsales"
-        comm_col: str = "rep1commission"
+        customer: str = "customer.1"
+        city: str = "shiptocity"
+        state: str = "shptostate"
+        sales: str = "netsales"
+        commission: str = "rep1commission"
 
-        # convert dollars to cents to avoid demical imprecision
-        data.loc[:,inv_col] *= 100
-        data.loc[:,comm_col] *= 100
-
-        ref_col = data.columns.tolist()[0]
-        data.dropna(subset=ref_col, inplace=True)
+        data = self.check_headers_and_fix([customer, city, state, sales, commission], data)
+        data.dropna(subset=customer, inplace=True)
+        data.loc[:,sales] *= 100
+        data.loc[:,commission] *= 100
 
         # sum by account convert to a flat table
-        piv_table_values = [inv_col, comm_col]
-        piv_table_index = [customer_name_col,city_name_col,state_name_col,"customer","shipto"]
+        piv_table_values = [sales, commission]
+        piv_table_index = [customer,city,state,"customer","shipto"]
         result = pd.pivot_table(
             data,
             values=piv_table_values,
@@ -45,8 +43,8 @@ class PreProcessor(AbstractPreProcessor):
         result = result.drop(columns=["customer","shipto"])
         result = result.apply(self.upper_all_str)
 
-        result["id_string"] = result[[customer_name_col, city_name_col, state_name_col]].apply("_".join, axis=1)
-        result.columns = ["customer", "city", "state", "inv_amt", "comm_amt", "id_string"]
+        result["id_string"] = result[[customer, city, state]].apply("_".join, axis=1)
+        result = result[['id_string', sales, commission]].rename(columns={sales: "inv_amt", commission: "comm_amt"})
         return PreProcessedData(result)
 
 
@@ -56,130 +54,134 @@ class PreProcessor(AbstractPreProcessor):
         Only the the sums of sales and commissions are used, 
             and they are reported as a single entry of negative amounts
         """
-        comm_col_before = data.columns.values[-1]
-        comm_col_after = "commission"
-        data = data.rename(columns={comm_col_before: comm_col_after}) # if commission rate changes, this column name would change
-        data.columns = [col.replace(" ","").lower() for col in data.columns.tolist() if isinstance(col,str)]
+        customer: str = kwargs.get('specified_customer','customer')
+        sales = "amount"
+        store_id = "branch"
+        location = "location"
 
+        headers = [sales, store_id, location]
+        data = self.check_headers_and_fix(headers,data)
 
-        na_filter_col = "date"
-        branch_total_amount_filter = "amount"
-        key_cols = ["amount","branch","location","commission"]
-        data = self.check_headers_and_fix(key_cols[:3],data)
-        data = data[data[na_filter_col].isna()].loc[:,data.columns.isin(key_cols)]
-        data = data[~data[branch_total_amount_filter].isna()]
+        commission_i = data.columns.values[-1]
+        commission = "comm_amt"
+        data = data.rename(columns={commission_i: commission}) # if commission rate changes, this column name would change
 
-        def strip_branch_number(value: str) -> int:
-            return value.lower().strip().replace(" total","")
+        data = data[data["date"].isna()]
+        data = data[headers+[commission]]
+        data = data.dropna(subset=sales)
 
-        data["branch"] = data["branch"].apply(strip_branch_number).astype(int)
-
-        # convert dollars to cents to avoid demical imprecision
-        data.loc[:,"amount"]= data["amount"].fillna(0).apply(lambda amt: amt*100)
-        data.loc[:,"commission"] = data["commission"].fillna(0).apply(lambda amt: amt*100)
+        data.loc[:, sales] = data[sales].fillna(0) * 100
+        data.loc[:, commission] = data[commission].fillna(0) * 100
 
         # amounts are negative to represent deductions from Coburn DC shipments
-        total_inv_adj = -data["amount"].sum()
-        total_comm_adj = -data["commission"].sum()
+        total_sales = -data[sales].sum()
+        total_commission = -data[commission].sum()
 
-        cols = ["customer", "inv_amt", "comm_amt", "id_string"]
-        result = pd.DataFrame([["COBURN",total_inv_adj, total_comm_adj]],
-            columns=cols[:-1])
-
+        cols = ["id_string", "inv_amt", commission]
+        result = pd.DataFrame([[customer, total_sales, total_commission]], columns=cols)
         result = result.apply(self.upper_all_str)
-
-        result["id_string"] = result[cols[0]]
-        result.columns = cols
 
         return PreProcessedData(result)
 
 
     def _re_michel_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
-        """
-        Process any tab of the report for RE Michel
-        The data provides branch numbers as well as locations
-        Branch numbers are stored in the database and should be used 
-            as the preferred method for matching to database customer entities
-        Locations in the form of city-state could be parsed out, but with more work
-            and prone to run into errors.
 
-        Returns a dataset with branch number, customer name (a default value), 
-            and amounts per branch
-
-        """
+        customer = kwargs.get('specified_customer', 'customer')
+        sales = "inv_amt"
+        commission = "comm_amt"
         split: float = kwargs.get("split", 1.0)
         comm_rate: float = kwargs.get("standard_commission_rate",0)
 
+        data = self.check_headers_and_fix(cols="cost", df=data)
+
         data = data.dropna(subset=data.columns[0])
         data = data.dropna(axis=1, how='all')
-        data.loc[:,"store_number"] = data.pop("branch").astype(str)
-        data.loc[:,"inv_amt"] = data.pop("cost")*split*100
-        data.loc[:,"comm_amt"] = data["inv_amt"]*comm_rate
-        data.loc[:,"customer"] = "RE MICHEL"
-        result = data.loc[:,["store_number", "customer", "inv_amt", "comm_amt"]]
-        result["id_string"] = result[["store_number", "customer"]].apply("_".join, axis=1)
+        data.loc[:, sales] = data.pop("cost")*split*100
+        data.loc[:, commission] = data[sales]*comm_rate
+        data.loc[:,"customer"] = customer
+        result = data.loc[:,["customer", sales, commission]]
+        result = result.rename(columns={"customer": "id_string"})
         return PreProcessedData(result)
         
 
     def _lennox_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
         
+        customer = kwargs.get('specified_customer', 'customer')
+        sales = "inv_amt"
+        commission = "comm_amt"
+        rec_city = "receiving_city"
+        rec_state = "receiving_state"
+        receiving = "receiving"
+        send_city = "sending_city"
+        send_state = "sending_state"
+        sending = "sending"
+        
         data = data.dropna(subset=data.columns[0])
         data = data.rename(columns={
-            "plnt": "receiving",
-            "city": "receiving_city",
-            "rg": "receiving_state",
-            "splt": "sending"
+            "plnt": receiving,
+            "city": rec_city,
+            "rg": rec_state,
+            "splt": sending
         })
+
+        def split_sender_location(value: str) -> pd.Series:
+            city, state = value.split(', ')
+            result = {
+                send_city: city,
+                send_state: state
+            }
+            return pd.Series(result)
+
         data = data.merge(
-            data["warehouse"].apply(
-                lambda value: pd.Series({"sending_city": value.split(", ")[0], "sending_state": value.split(", ")[1]})
-            ),
+            data["warehouse"].apply(split_sender_location),
             left_index=True,
             right_index=True
         )
-        data.loc[:,"inv_amt"] = data["extprice"]*100
-        data.loc[:,"comm_amt"] = data["commission"]*100
-        data.loc[:,"customer"] = "LENNOX"
-        result_cols = ["receiving_city", "receiving_state", "sending_city", "sending_state",
-                       "customer", "inv_amt", "comm_amt", "receiving", "sending"]
+        data.loc[:, sales] = data["extprice"]*100
+        data.loc[:, commission] = data["commission"]*100
+        data.loc[:, "customer"] = customer
+        result_cols = [rec_city, rec_state, send_city, send_state,
+                       "customer", sales, commission, receiving, sending]
         result = data.loc[:,result_cols]
         
-        # unpivot sending and receiving (trailing columns) into "direction" (sending/receiving) and warehouse code (i.e. A300)
+        # split sending and receiving warehouses, assigning negative values to the sender, and then combine the tables
         result = pd.melt(result,id_vars=result_cols[:-2],var_name="direction",value_name="store_number")
-        # sending table only
+        # sending table
         sending_table = result.loc[
-            result["direction"] == "sending", 
-            ["sending_city", "sending_state", "customer", "inv_amt", "comm_amt", "direction", "store_number"]
+            result["direction"] == sending, 
+            [send_city, send_state, "customer", sales, commission, "direction", "store_number"]
         ]
-        sending_table = sending_table.rename(columns={"sending_city": "city", "sending_state": "state"})
-        sending_table["inv_amt"] *= -1
-        sending_table["comm_amt"] *= -1
-        # receiving table only
+        sending_table = sending_table.rename(columns={send_city: "city", send_state: "state"})
+        sending_table[sales] *= -1
+        sending_table[commission] *= -1
+        # receiving table
         receiving_table = result.loc[
-            result["direction"] == "receiving", 
-            ["receiving_city", "receiving_state", "customer", "inv_amt", "comm_amt", "direction", "store_number"]
+            result["direction"] == receiving, 
+            [rec_city, rec_state, "customer", sales, commission, "direction", "store_number"]
         ]
-        receiving_table = receiving_table.rename(columns={"receiving_city": "city", "receiving_state": "state"})
+        receiving_table = receiving_table.rename(columns={rec_city: "city", rec_state: "state"})
         # recombine 
         result = pd.concat([sending_table,receiving_table], ignore_index=True)
+        assert result[sales].sum() == 0, "sales values do not add up to zero"
+        assert result[commission].sum() == 0, "commission values do not add up to zero"
         result = result.apply(self.upper_all_str)
-        result["id_string"] = result[["store_number", "customer", "city", "state"]].apply("_".join, axis=1)
-        result = result[['id_string','inv_amt','comm_amt']]
+        result["id_string"] = result[["customer", "city", "state"]].apply("_".join, axis=1)
+        result = result[['id_string', sales, commission]]
         return PreProcessedData(result)
 
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "detail": (self._standard_report_preprocessing,0),
-            "coburn_pos": (self._coburn_report_preprocessing,0),
-            "lennox_pos": (self._lennox_report_preprocessing,0),
-            "re_michel_pos": (self._re_michel_report_preprocessing,2)
+            "detail": self._standard_report_preprocessing,
+            "coburn_pos": self._coburn_report_preprocessing,
+            "lennox_pos": self._lennox_report_preprocessing,
+            "re_michel_pos": self._re_michel_report_preprocessing
         }
-        preprocess_method, skip = method_by_name.get(self.report_name, None)
+        preprocess_method = method_by_name.get(self.report_name, None)
         if preprocess_method:
             if self.report_name == "lennox_pos":
-                return preprocess_method(self.file.to_df(combine_sheets=True, skip=skip, treat_headers=True), **kwargs)
-            return preprocess_method(self.file.to_df(skip=skip, treat_headers=True), **kwargs)
+                return preprocess_method(self.file.to_df(combine_sheets=True, treat_headers=True), **kwargs)
+            return preprocess_method(self.file.to_df(treat_headers=True), **kwargs)
         else:
             return
 

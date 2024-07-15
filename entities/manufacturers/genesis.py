6a6518433c8e865f25f2e4f939cb9064f5892a76
carboni-joseph <joe@carbonitech.com>
2024-07-15 13:27:11 -0400

    genesis standard and winsupply now use the column name options from the db

--- entities/manufacturers/genesis.py
+++ entities/manufacturers/genesis.py
@@ -2,34 +2,49 @@
 Manufacturer report preprocessing definition
 for Genesis Cable / Resideo
 """
+
 import pandas as pd
 from entities.commission_data import PreProcessedData
 from entities.preprocessor import AbstractPreProcessor
 
+
 class PreProcessor(AbstractPreProcessor):
 
-    def _standard_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _standard_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
         """processes the 'Sales Detail' genesis file"""
 
-        customer_name_col: str = "custname"
-        city_name_col: str = "city"
-        state_name_col: str = "state"
-        inv_col: str = "amount"
-        comm_col: str = "commdue"
+        data, cols = self.use_column_options(data, **kwargs)
 
-        data = data.dropna(subset=data.columns.to_list()[0])
-        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
-        result.loc[:,inv_col] *= 100
-        result.loc[:,comm_col] *= 100
+        if not cols:
+            raise Exception(
+                "column names discoverable in the data do not match"
+                " any of the options given"
+            )
+
+        customer_name_col: str = cols["customer"]
+        city_name_col: str = cols["city"]
+        state_name_col: str = cols["state"]
+        inv_col: str = cols["sales"]
+        comm_col: str = cols["commissions"]
+
+        data = data.dropna(subset=cols["state"])
+        result = data.loc[
+            :, [customer_name_col, city_name_col, state_name_col, inv_col, comm_col]
+        ]
+        result.loc[:, inv_col] *= 100
+        result.loc[:, comm_col] *= 100
         col_names = ["customer", "city", "state", "inv_amt", "comm_amt"]
         result.columns = col_names
         result = result.apply(self.upper_all_str)
         result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
-        result = result[['id_string', 'inv_amt', 'comm_amt']]
+        result = result[["id_string", "inv_amt", "comm_amt"]]
         return PreProcessedData(result)
 
-
-    def _baker_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _baker_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
         """
         processes the 'Baker' genesis file
         Remarks:
@@ -44,85 +59,119 @@ class PreProcessor(AbstractPreProcessor):
         comm_col: str = "comm.due"
 
         split: float = kwargs.get("split", 1.0)
-        branch_proportions: pd.DataFrame = kwargs.get("customer_proportions_by_state", pd.DataFrame())
-        comm_ref_col: str   # will set which column to use for the commission calculation
+        branch_proportions: pd.DataFrame = kwargs.get(
+            "customer_proportions_by_state", pd.DataFrame()
+        )
+        comm_ref_col: str  # will set which column to use for the commission calculation
 
         if kwargs.get("lennox"):
             alt_split = 0.01
             comm_ref_col = inv_col
         else:
-            alt_split = 100*split
+            alt_split = 100 * split
             comm_ref_col = comm_col
 
-        data = data.dropna(axis=1, how='all')
+        data = data.dropna(axis=1, how="all")
         data = data.dropna(subset=data.columns.to_list()[0])
-        data = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
-        data.loc[:,inv_col] = data[inv_col]*100*split
-        data.loc[:,comm_col] = data[comm_ref_col]*alt_split
-        
+        data = data.loc[
+            :, [customer_name_col, city_name_col, state_name_col, inv_col, comm_col]
+        ]
+        data.loc[:, inv_col] = data[inv_col] * 100 * split
+        data.loc[:, comm_col] = data[comm_ref_col] * alt_split
+
         # Locations in the data are not territory-specific although they're used to calculate the values.
         # Total sales and commissions are split between states in-territory based on total proportion of branch count
         total_inv = data[inv_col].sum()
         total_comm = data[comm_col].sum()
         if not branch_proportions.empty:
-            branch_proportions["inv_amt"] = branch_proportions["total_share"]*total_inv
-            branch_proportions["comm_amt"] = branch_proportions["total_share"]*total_comm
-            branch_proportions['id_string'] = branch_proportions[['customer', 'state']].apply('_'.join, axis=1)
-            result = branch_proportions[['id_string', 'inv_amt', 'comm_amt']]
+            branch_proportions["inv_amt"] = (
+                branch_proportions["total_share"] * total_inv
+            )
+            branch_proportions["comm_amt"] = (
+                branch_proportions["total_share"] * total_comm
+            )
+            branch_proportions["id_string"] = branch_proportions[
+                ["customer", "state"]
+            ].apply("_".join, axis=1)
+            result = branch_proportions[["id_string", "inv_amt", "comm_amt"]]
         else:
             specified_customer = self.get_customer(**kwargs)
-            result = pd.DataFrame([[specified_customer, total_inv, total_comm]], columns=['id_string', 'inv_amt', 'comm_amt'])
+            result = pd.DataFrame(
+                [[specified_customer, total_inv, total_comm]],
+                columns=["id_string", "inv_amt", "comm_amt"],
+            )
         return PreProcessedData(result)
 
-
-    def _lennox_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _lennox_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
         """
-            Processing is nearly identical to Baker's report, except minor tweaks to the use
-            of commission split values. The tables have the same structure and headings after extraction
+        Processing is nearly identical to Baker's report, except minor tweaks to the use
+        of commission split values. The tables have the same structure and headings after extraction
 
-            Setting `lennox=True` will induce the following method to use commission column calculations
-            appropriate for the Lennox report
+        Setting `lennox=True` will induce the following method to use commission column calculations
+        appropriate for the Lennox report
         """
         return self._baker_report_preprocessing(data, lennox=True, **kwargs)
 
-    def _winsupply_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
-        
-        customer_name_col: str = "billtoaddress1"
-        city_name_col: str = "billtoaddress4"
-        city_name_alt: str = "billtoaddress3"
-        state_name_col: str = "billtostate"
-        inv_col: str = "mtdsales"
-        comm_col: str = "comm_amt" 
-        comm_col_index: int = -1 # commission column has an unreliable name
+    def _winsupply_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
 
-        data = data.dropna(axis=1, how='all')
-        data = data.dropna(subset=data.columns.to_list()[0])
-        data.loc[:,comm_col] = data.iloc[:,comm_col_index]
+        data, cols_used = self.use_column_options(data, **kwargs)
+
+        if cols_used["city"][-1] == "4":
+            city_name_alt = cols_used["city"][:-1] + "3"
+        else:
+            city_name_alt = cols_used["city"][:-1] + "4"
+
+        customer_name_col: str = cols_used["customer"]
+        city_name_col: str = cols_used["city"]
+        city_name_alt: str = city_name_alt
+        state_name_col: str = cols_used["state"]
+        inv_col: str = cols_used["sales"]
+        comm_col = cols_used.get("commissions")
+
+        data = data.dropna(axis=1, how="all")
+        data = data.dropna(subset=state_name_col)
+
+        if not comm_col:
+            comm_col: str = "comm_amt"
+            comm_col_index: int = -1  # commission column has an unreliable name
+            data.loc[:, comm_col] = data.iloc[:, comm_col_index]
+        else:
+            data.loc[:, "comm_amt"] = data.loc[:, comm_col]
+            comm_col: str = "comm_amt"
+
         data[city_name_col].fillna(data[city_name_alt], inplace=True)
-        result = data.loc[:,[customer_name_col, city_name_col, state_name_col, inv_col, comm_col]]
-        result.loc[:,inv_col] *= 100
-        result.loc[:,comm_col] *= 100
+        result = data.loc[
+            :, [customer_name_col, city_name_col, state_name_col, inv_col, comm_col]
+        ]
+        result.loc[:, inv_col] *= 100
+        result.loc[:, comm_col] *= 100
         col_names = ["customer", "city", "state", "inv_amt", "comm_amt"]
         result.columns = col_names
         result = result.apply(self.upper_all_str)
         result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
         return PreProcessedData(result)
 
-    def _rebate_detail_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _rebate_detail_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
 
-        customer_name_col: str = 'customername'
+        customer_name_col: str = "customername"
         customer_name_index: int = data.columns.to_list().index(customer_name_col)
         inv_col_index: int = -2
         comm_col_index: int = -1
 
-        result = data.iloc[:,[customer_name_index, inv_col_index, comm_col_index]]
+        result = data.iloc[:, [customer_name_index, inv_col_index, comm_col_index]]
 
-        data = data.dropna(axis=1, how='all')
+        data = data.dropna(axis=1, how="all")
         data = data.dropna(subset=data.columns.to_list()[0])
 
         # negating (cent) values to reflect rebate as a refund
-        result.iloc[:,inv_col_index] *= -100
-        result.iloc[:,comm_col_index] *= -100
+        result.iloc[:, inv_col_index] *= -100
+        result.iloc[:, comm_col_index] *= -100
 
         result = result.apply(self.upper_all_str)
 
@@ -132,39 +181,47 @@ class PreProcessor(AbstractPreProcessor):
 
         return PreProcessedData(result)
 
-    def _johnstone_report_preprocessing(self, data: pd.DataFrame, **kwargs) -> PreProcessedData:
+    def _johnstone_report_preprocessing(
+        self, data: pd.DataFrame, **kwargs
+    ) -> PreProcessedData:
 
         customer: str = self.get_customer(**kwargs)
         city: str = "storename"
         state: str = "storestate"
         sales: str = "lastmocogs"
-        commission: str = 'comm_amt'
-        comm_rate: float = kwargs.get('standard_commission_rate', 0)
+        commission: str = "comm_amt"
+        comm_rate: float = float(kwargs.get("standard_commission_rate", 0))
 
         data = self.check_headers_and_fix([city, state, sales], data)
         data = data.dropna(subset=city)
         data.loc[:, "customer"] = customer
+        data[sales] = data[sales].astype(float)
         data[sales] *= 100
         data[commission] = data[sales] * comm_rate
+        data[commission] = data[commission].astype(float)
         data["id_string"] = data[["customer", city, state]].apply("_".join, axis=1)
-        result = data[["id_string", sales, commission]].rename(columns={sales: "inv_amt"})
+        result = data[["id_string", sales, commission]].rename(
+            columns={sales: "inv_amt"}
+        )
         return PreProcessedData(result)
-
-
 
     def preprocess(self, **kwargs) -> PreProcessedData:
         method_by_name = {
-            "sales_detail": (self._standard_report_preprocessing,0),
-            "baker_pos": (self._baker_report_preprocessing,33),
-            "lennox_pos": (self._lennox_report_preprocessing,45),
-            "winsupply_pos": (self._winsupply_report_preprocessing,0),
-            "rebate_detail": (self._rebate_detail_report_preprocessing,0),
-            "johnstone_pos": (self._johnstone_report_preprocessing,0)
+            "sales_detail": (self._standard_report_preprocessing, 0),
+            "baker_pos": (self._baker_report_preprocessing, 33),
+            "lennox_pos": (self._lennox_report_preprocessing, 45),
+            "winsupply_pos": (self._winsupply_report_preprocessing, 0),
+            "rebate_detail": (self._rebate_detail_report_preprocessing, 0),
+            "johnstone_pos": (self._johnstone_report_preprocessing, 0),
         }
         preprocess_method, skip_param = method_by_name.get(self.report_name, None)
         if preprocess_method:
             if self.report_name == "winsupply_pos":
-                return preprocess_method(self.file.to_df(combine_sheets=True, treat_headers=True), **kwargs)
-            return preprocess_method(self.file.to_df(skip=skip_param, treat_headers=True), **kwargs)
+                return preprocess_method(
+                    self.file.to_df(combine_sheets=True, treat_headers=True), **kwargs
+                )
+            return preprocess_method(
+                self.file.to_df(skip=skip_param, treat_headers=True), **kwargs
+            )
         else:
             return
--- entities/preprocessor.py
+++ entities/preprocessor.py
@@ -4,14 +4,11 @@ from pandas import Series, DataFrame
 from entities.commission_data import PreProcessedData
 from entities.commission_file import CommissionFile
 
+
 class AbstractPreProcessor(ABC):
 
-    EXPECTED_TYPES = {
-        'id_string': object,
-        'inv_amt': float,
-        'comm_amt': float
-    }
-    
+    EXPECTED_TYPES = {"id_string": object, "inv_amt": float, "comm_amt": float}
+
     def __init__(self, report_name: str, submission_id: int, file: CommissionFile):
         self.report_name = report_name
         self.submission_id = submission_id
@@ -30,49 +27,69 @@ class AbstractPreProcessor(ABC):
     @staticmethod
     def get_customer(**kwargs) -> str:
         """Get the specified customer sent as specified_customer
-        
-            If found, the customer will 
-            be represented as a `tuple[int,str]`,
-            where `int` is the customer id
-            and `str` is the customer name.
-        
-            The customer name is returned.
+
+        If found, the customer will
+        be represented as a `tuple[int,str]`,
+        where `int` is the customer id
+        and `str` is the customer name.
+
+        The customer name is returned.
         """
-        if customer := kwargs.get('specified_customer', None):
+        if customer := kwargs.get("specified_customer", None):
             return customer[1]
         else:
-            return 'customer'
+            return "customer"
 
-
-    
     @staticmethod
-    def check_headers_and_fix(cols: str|list[str], df: DataFrame) -> DataFrame:
+    def check_headers_and_fix(
+        cols: str | list[str], df: DataFrame, indicate: bool = False
+    ) -> DataFrame | tuple[DataFrame, bool]:
         """check that a dataframe's headers contain the column name(s)
         supplied in cols. If the dataframe columns do not match,
         iterate through the rows to find the first row with column headers
         and set the dataframe as if all prior rows had been skipped upon loading
-        
+
         if the rows never reveal column headers, the original dataframe is returned
 
         Returns: DataFrame"""
 
+        indicator = False
         if isinstance(cols, str):
             cols = [cols]
         # check that my columns contain the headers I expect
-        df = df.rename(columns=lambda col: str(col).lower().replace(' ','')) 
-        if not set(cols) & set(df.columns):
+        df = df.rename(columns=lambda col: str(col).lower().replace(" ", ""))
+        if not set(cols) <= set(df.columns):
             # iterate through the rows until we find the column header
             for index, row in df.iterrows():
-                row_vals = [str(value).lower().replace(' ','') for value in row.values.tolist()]
-                if set(cols) & set(row_vals):
+                row_vals = [
+                    str(value).lower().replace(" ", "") for value in row.values.tolist()
+                ]
+                if set(cols) <= set(row_vals):
                     # set the df to use the column row as header
                     # and next row is the first row
                     df.columns = row_vals
-                    df = df.iloc[index+1:]
+                    df = df.iloc[index + 1 :]
+                    indicator = True
                     break
-        return df
+        else:
+            indicator = True
+        return (df, indicator) if indicate else df
 
-        
+    def use_column_options(self, data: DataFrame, **kwargs) -> dict[str, str]:
+        column_name_options: list[dict] = kwargs.get("column_names")
+        cols_used = {}
+        for names_option in column_name_options:
+            names_option_stripped = [
+                name for name in list(names_option.values())[2:] if name
+            ]
+            data, cols_were_used = self.check_headers_and_fix(
+                names_option_stripped, data, indicate=True
+            )
+            if cols_were_used:
+                cols_used = names_option
+                break
+        return data, cols_used
+
     @abstractmethod
-    def preprocess(self, **kwargs)-> PreProcessedData:
+    def preprocess(self, **kwargs) -> PreProcessedData:
         pass

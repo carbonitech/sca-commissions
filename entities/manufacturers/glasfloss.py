98f66c15caaf46fa8e3aa2804169ad093137cd86
carboni-joseph <joe@carbonitech.com>
2024-10-01 11:07:08 -0400

    use new column name approach using db table, simplify names, light refactoring

--- entities/manufacturers/glasfloss.py
+++ entities/manufacturers/glasfloss.py
@@ -13,44 +13,41 @@ class PreProcessor(AbstractPreProcessor):
     def _standard_report_preprocessing(
         self, data: pd.DataFrame, **kwargs
     ) -> PreProcessedData:
-        """processes the standard Glasfloss file"""
 
-        customer_name_col: str = "shiptoname"
-        city_name_col: str = "shiptocity"
-        state_name_col: str = "shiptostate"
-        inv_col: str = "sumofsales"
-        comm_col: str = "comm_amt"
-        total_freight: float = kwargs.get("total_freight_amount", None)
-        total_rebate_credits: float = kwargs.get("total_rebate_credits", None)
+        data, cols = self.use_column_options(data, **kwargs)
+
+        customer: str = cols["customer"]
+        city: str = cols["city"]
+        state: str = cols["state"]
+        sale: str = cols["sales"]
+        commission: str = "comm_amt"
         comm_rate = kwargs.get("standard_commission_rate", 0)
 
-        data = self.check_headers_and_fix(
-            [customer_name_col, city_name_col, state_name_col, inv_col], data
-        )
-        data = data.dropna(subset=data.columns.to_list()[1])
-        data.loc[:, inv_col] *= 100
+        total_freight: float = kwargs.get("total_freight_amount", None)
+        total_rebate_credits: float = kwargs.get("total_rebate_credits", None)
+
+        data = data.dropna(subset=data.columns[1])
+        data.loc[:, sale] *= 100
 
         if total_rebate_credits:
-            total_sales = data[inv_col].sum() / 100  # converted to dollars
+            total_sales = data[sale].sum() / 100  # converted to dollars
             discount_rate = total_rebate_credits / total_sales
-            data.loc[:, inv_col] = data[inv_col] * (1 - discount_rate)
+            data.loc[:, sale] = data[sale] * (1 - discount_rate)
 
         if total_freight:
-            total_sales = data[inv_col].sum() / 100  # converted to dollars
+            total_sales = data[sale].sum() / 100  # converted to dollars
             discount_rate = total_freight / total_sales
-            data.loc[:, inv_col] = data[inv_col] * (1 - discount_rate)
-            data.loc[:, comm_col] = data[inv_col] * comm_rate
+            data.loc[:, sale] = data[sale] * (1 - discount_rate)
+            data.loc[:, commission] = data[sale] * comm_rate
         else:
-            data.loc[:, comm_col] = 0
+            data.loc[:, commission] = 0
 
-        result = data.loc[
-            :, [customer_name_col, city_name_col, state_name_col, inv_col, comm_col]
-        ]
-        col_names = ["customer", "city", "state", "inv_amt", comm_col]
+        result = data.loc[:, [customer, city, state, sale, commission]]
+        col_names = ["customer", "city", "state", "inv_amt", commission]
         result.columns = col_names
         result = result.apply(self.upper_all_str)
         result["id_string"] = result[col_names[:3]].apply("_".join, axis=1)
-        result = result[["id_string", "inv_amt", comm_col]]
+        result = result[["id_string", "inv_amt", commission]]
         result = result.astype(self.EXPECTED_TYPES)
         return PreProcessedData(result)
 

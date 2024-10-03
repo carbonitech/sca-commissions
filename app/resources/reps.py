7dfcd4a93346f3a99694f72cd79e5692b6d4e2b0
carboni-joseph <joe@carbonitech.com>
2024-10-03 11:00:25 -0400

    formatting

--- app/resources/reps.py
+++ app/resources/reps.py
@@ -7,47 +7,52 @@ from services.utils import User, get_db, get_user
 
 router = APIRouter(prefix="/representatives", route_class=JSONAPIRoute)
 
+
 @router.get("", tags=["reps"])
 async def all_reps(
-        query: Query=Depends(),
-        db: Session=Depends(get_db),
-        user: User=Depends(get_user)
-    ):
+    query: Query = Depends(),
+    db: Session = Depends(get_db),
+    user: User = Depends(get_user),
+):
     jsonapi_query = convert_to_jsonapi(query)
-    return get.reps(db,jsonapi_query,user)
+    return get.reps(db, jsonapi_query, user)
+
 
 @router.get("/{rep_id}", tags=["reps"])
 async def rep_by_id(
-        rep_id: int,
-        query: Query=Depends(),
-        db: Session=Depends(get_db),
-        user: User=Depends(get_user)
-    ):
+    rep_id: int,
+    query: Query = Depends(),
+    db: Session = Depends(get_db),
+    user: User = Depends(get_user),
+):
     jsonapi_query = convert_to_jsonapi(query)
-    return get.reps(db,jsonapi_query,user,rep_id)
+    return get.reps(db, jsonapi_query, user, rep_id)
+
 
 @router.post("", tags=["reps"])
 async def add_new_rep(
-        jsonapi_obj: RequestModels.new_representative,
-        db: Session=Depends(get_db),
-        user: User=Depends(get_user)
-    ):
-    return post.representative(db,json_data=jsonapi_obj.dict(exclude_none=True), user=user)
+    jsonapi_obj: RequestModels.new_representative,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_user),
+):
+    return post.representative(
+        db, json_data=jsonapi_obj.dict(exclude_none=True), user=user
+    )
+
 
 @router.patch("/{rep_id}", tags=["reps"])
 async def modify_a_rep(
-        rep_id: int,
-        rep_data: RequestModels.rep_modification,
-        db: Session=Depends(get_db),
-        user: User=Depends(get_user)
-    ):
+    rep_id: int,
+    rep_data: RequestModels.rep_modification,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_user),
+):
     return patch.representative(db, rep_id, rep_data.dict(), user)
+
 
 @router.delete("/{rep_id}", tags=["reps"])
 async def delete_rep(
-        rep_id: int,
-        db: Session=Depends(get_db),
-        user: User=Depends(get_user)
-    ):
+    rep_id: int, db: Session = Depends(get_db), user: User = Depends(get_user)
+):
     # soft delete
     return delete.representative(db=db, rep_id=rep_id, user=user)

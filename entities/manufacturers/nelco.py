498ca970a56ffeff86cad21bd3b7d67e107318e5
carboni-joseph <joe@carbonitech.com>
2024-10-07 12:11:21 -0400

    implement strategy for different sorting method of data in the document. use dynamically based on a "Sorted By" field in the data

--- entities/manufacturers/nelco.py
+++ entities/manufacturers/nelco.py
@@ -19,6 +19,81 @@ class PreProcessor(AbstractPreProcessor):
         heavy amount of parsing required to get to the data, which comes in as one long series
         """
 
+        for i, value in data.items():
+            if value == "Sorted By :":
+                sort_strategy = data.loc[i + 1]
+                match sort_strategy:
+                    case "Salesman":
+                        return self._sorted_by_salesman_strategy(data, **kwargs)
+                    case "Customer":
+                        return self._sorted_by_customer_strategy(data, **kwargs)
+
+    def _sorted_by_salesman_strategy(
+        self, data: pd.Series, **kwargs
+    ) -> PreProcessedData:
+        invoice_date = r"\d{2}/\d{2}/\d{2}"
+        comm_rate: float = kwargs.get("standard_commission_rate")
+
+        # grab first text element of PDF page header
+        data_printed_date = data.iloc[0]
+        # remove all text elements matching the header elements in the first 42 text elements
+        data_cleaned = data.loc[~data.isin(data[:42])].str.replace(
+            data_printed_date, ""
+        )
+        # remove page number lines using regex
+        data_cleaned = data_cleaned[
+            ~data_cleaned.str.contains(r"PAGE:\s*\d")
+        ].reset_index(drop=True)
+        invoice_boundaries: list[int] = data_cleaned.loc[
+            data_cleaned.str.match(invoice_date)
+        ].index.tolist()
+        compiled_data = {
+            "customer": [],
+            "city": [],
+            "state": [],
+            "inv_amt": [],
+            # adding comm_amt later
+        }
+        for i, index in enumerate(invoice_boundaries):
+            if i == len(invoice_boundaries) - 1:
+                # last customer, go to the end of the dataset, except grand totals
+                subseries = data_cleaned.iloc[index:-3]
+            else:
+                subseries = data_cleaned.iloc[index : invoice_boundaries[i + 1]]
+
+            customer = subseries.iloc[3]
+            city = ""
+            state = subseries.iloc[4]
+            inv_label_index = subseries[
+                subseries.str.match("Invoice Totals")
+            ].index.item()
+            inv_amt = float(subseries.loc[inv_label_index + 2])
+
+            compiled_data["customer"].append(customer)
+            compiled_data["city"].append(city)
+            compiled_data["state"].append(state)
+            compiled_data["inv_amt"].append(inv_amt)
+
+        result = pd.DataFrame(compiled_data)
+        result.loc[:, "inv_amt"] *= 100
+        result.loc[:, "comm_amt"] = result.loc[:, "inv_amt"] * comm_rate
+        result = result.apply(self.upper_all_str)
+        result["id_string"] = result[["customer", "city", "state"]].apply(
+            "_".join, axis=1
+        )
+        result = result[["id_string", "inv_amt", "comm_amt"]]
+        result = result.astype(self.EXPECTED_TYPES)
+        return PreProcessedData(result)
+
+    def _sorted_by_customer_strategy(
+        self, data: pd.Series, **kwargs
+    ) -> PreProcessedData:
+        """processes the standard Nelco file
+
+        this report comes in as a PDF and looks more like a sales report
+        heavy amount of parsing required to get to the data, which comes in as one long series
+        """
+
         customer_name: int = 2
         city_state_regex: str = (
             r"^([^().\d/_]+?),\s*?(\w{2})"  # searches for format like Atlanta, GA at beginning of str
@@ -76,11 +151,10 @@ class PreProcessor(AbstractPreProcessor):
 
         col_names = ["customer", "city", "state", "inv_amt", "comm_amt"]
         result.columns = col_names
-        print(result)
         result["id_string"] = result[col_names[:3]].apply(
             "_".join, axis=1
         )  # empty city col makes this 'customer__state'
-        result = result[result.columns[-3:]]
+        result = result[result.columns[-3:]].astype(self.EXPECTED_TYPES)
         return PreProcessedData(result)
 
     def preprocess(self, **kwargs) -> PreProcessedData:
